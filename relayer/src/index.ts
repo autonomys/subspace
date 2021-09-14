0dba5fd4332f525de3ff36b1694faf6917447332
Serge <isserge@protonmail.com>
2021-09-14 12:12:29 +0400

    relayer: refactoring + types

--- relayer/src/index.ts
+++ relayer/src/index.ts
@@ -1,4 +1,7 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
+import { Header } from "@polkadot/types/interfaces";
+import { RegistryTypes } from "@polkadot/types/types";
+import { AddressOrPair } from "@polkadot/api/submittable/types";
 import { merge } from "rxjs";
 import { concatMap, map } from "rxjs/operators";
 
@@ -10,59 +13,73 @@ const types = {
   PutDataObject: "Vec<u8>",
 };
 
-// TODO: remove IIFE when Eslint is updated to v8.0.0 (will support top-level await)
-(async () => {
-  const targetProvider = new WsProvider(config.targetChainUrl);
-
-  const targetApi = await ApiPromise.create({
-    provider: targetProvider,
+const createApi = async (url: string, types?: RegistryTypes) => {
+  const provider = new WsProvider(url);
+  const api = await ApiPromise.create({
+    provider,
     types,
   });
 
+  return api;
+};
+
+const createGetBlockByHeader =
+  (api: ApiPromise) =>
+  async ({ hash }: Header) => {
+    const chain = await api.rpc.system.chain();
+    const block = await api.rpc.chain.getBlock(hash);
+    // TODO: should include size of headers?
+    // TODO: what is the size limit?
+    // TODO: check size - if too big reject
+    const size = Buffer.byteLength(JSON.stringify(block));
+
+    console.log(`Chain ${chain}: Finalized block hash: ${hash}`);
+    console.log(`Chain ${chain}: Finalized block size: ${size / 1024} Kb`);
+
+    // TODO: clarify how we identify chains
+    return JSON.stringify({ ...block.toJSON(), chain });
+  };
+
+const createSendBlockTx =
+  (api: ApiPromise, signer: AddressOrPair) => async (block: string) => {
+    const txHash = await api.tx.feeds
+      .put(block)
+      // it is required to specify nonce, otherwise transaction within same block will be rejected
+      // if nonce is -1 API will do the lookup for the right value
+      // https://polkadot.js.org/docs/api/cookbook/tx/#how-do-i-take-the-pending-tx-pool-into-account-in-my-nonce
+      .signAndSend(signer, { nonce: -1 });
+
+    return txHash;
+  };
+
+// TODO: remove IIFE when Eslint is updated to v8.0.0 (will support top-level await)
+(async () => {
+  const targetApi = await createApi(config.targetChainUrl, types);
+  // use getAccount func because we cannot create keyring instance before API is instanciated
+  const signer = getAccount(config.accountSeed);
+
+  const sendBlockTx = createSendBlockTx(targetApi, signer);
+
   const sourceApis = await Promise.all(
     config.sourceChainUrls.map(async (url) => {
-      const provider = new WsProvider(url);
-      const api = await ApiPromise.create({
-        provider,
-      });
-
+      const api = await createApi(url);
       return api;
     })
   );
 
-  // use getAccount func because we cannot create keyring instance before API is instanciated
-  const signer = getAccount(config.accountSeed);
-
-  // TODO: add old block processing
   const blockSubscriptions = sourceApis.map((api) => {
+    const getBlockByHeader = createGetBlockByHeader(api);
     // use pipe and concatMap to process events one by one
-    return api.rx.rpc.chain.subscribeFinalizedHeads().pipe(
-      concatMap(async ({ hash }) => {
-        const chain = await api.rpc.system.chain();
-        const block = await api.rpc.chain.getBlock(hash);
-        // TODO: should include size of headers?
-        // TODO: what is the size limit?
-        const size = Buffer.byteLength(JSON.stringify(block));
-
-        console.log(`Chain ${chain}: Finalized block hash: ${hash}`);
-        console.log(`Chain ${chain}: Finalized block size: ${size / 1024} Kb`);
-        // TODO: clarify how we identify chains
-        return JSON.stringify({ ...block.toJSON(), chain });
-      })
-    );
+    return api.rx.rpc.chain
+      .subscribeFinalizedHeads()
+      .pipe(concatMap(getBlockByHeader));
   });
 
   merge(...blockSubscriptions)
     // use pipe and concatMap to process events one by one
     .pipe(
-      concatMap(async (block) => {
-        // TODO: check size - if too big reject
-        const txHash = await targetApi.tx.feeds
-          .put(block)
-          // it is required to specify nonce, otherwise transaction within same block will be rejected
-          // if nonce is -1 API will do the lookup for the right value
-          // https://polkadot.js.org/docs/api/cookbook/tx/#how-do-i-take-the-pending-tx-pool-into-account-in-my-nonce
-          .signAndSend(signer, { nonce: -1 });
+      concatMap(sendBlockTx),
+      map((txHash) => {
         // TODO: clarify if we need to know which tx corresponds to which chain
         console.log(`Transaction sent: ${txHash}`);
       })
