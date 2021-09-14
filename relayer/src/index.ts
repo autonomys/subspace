076471e02ee935979cd99c41a014836ac5b57850
Serge <isserge@protonmail.com>
2021-09-14 11:29:52 +0400

    relayer: added block size

--- relayer/src/index.ts
+++ relayer/src/index.ts
@@ -40,9 +40,14 @@ const types = {
       concatMap(async ({ hash }) => {
         const chain = await api.rpc.system.chain();
         const block = await api.rpc.chain.getBlock(hash);
+        // TODO: should include size of headers?
+        // TODO: what is the size limit?
+        const size = Buffer.byteLength(JSON.stringify(block));
+
         console.log(`Chain ${chain}: Finalized block hash: ${hash}`);
+        console.log(`Chain ${chain}: Finalized block size: ${size / 1024} Kb`);
         // TODO: clarify how we identify chains
-        return { ...block.toJSON(), chain };
+        return JSON.stringify({ ...block.toJSON(), chain });
       })
     );
   });
@@ -53,12 +58,13 @@ const types = {
       concatMap(async (block) => {
         // TODO: check size - if too big reject
         const txHash = await targetApi.tx.feeds
-          .put(JSON.stringify(block))
+          .put(block)
           // it is required to specify nonce, otherwise transaction within same block will be rejected
           // if nonce is -1 API will do the lookup for the right value
           // https://polkadot.js.org/docs/api/cookbook/tx/#how-do-i-take-the-pending-tx-pool-into-account-in-my-nonce
           .signAndSend(signer, { nonce: -1 });
-        console.log(`Chain ${block.chain}: Transaction sent: ${txHash}`);
+        // TODO: clarify if we need to know which tx corresponds to which chain
+        console.log(`Transaction sent: ${txHash}`);
       })
     )
     .subscribe();
