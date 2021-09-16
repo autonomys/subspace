28b6aacc9ebc20ce3e3c21699f7f07a8380009fc
Serge <isserge@protonmail.com>
2021-09-16 15:16:53 +0400

    relayer: apply cargo fmt

--- node-template-spartan/runtime/src/lib.rs
+++ node-template-spartan/runtime/src/lib.rs
@@ -30,12 +30,12 @@ pub use frame_support::{
     StorageValue,
 };
 pub use pallet_balances::Call as BalancesCall;
+pub use pallet_feeds;
 pub use pallet_timestamp::Call as TimestampCall;
 use pallet_transaction_payment::CurrencyAdapter;
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
-pub use pallet_feeds;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -317,7 +317,7 @@ impl pallet_offences_poc::Config for Runtime {
 }
 
 impl pallet_feeds::Config for Runtime {
-	type Event = Event;
+    type Event = Event;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
