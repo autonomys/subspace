f18e39a0d905dadff806403559df2c2d11fce5fc
Serge <isserge@protonmail.com>
2021-09-17 11:31:19 +0400

    relayer: refactoring Source - added chain id, use TxData type

--- relayer/src/source.ts
+++ relayer/src/source.ts
@@ -1,39 +1,45 @@
 import { ApiPromise } from "@polkadot/api";
-import { Header, Hash, SignedBlock } from "@polkadot/types/interfaces";
+import { Header, Hash } from "@polkadot/types/interfaces";
 import { Observable } from "@polkadot/types/types";
-import { Text } from "@polkadot/types/primitive";
+import { Text, U32 } from "@polkadot/types/primitive";
 import { concatMap } from "rxjs/operators";
+
+import { TxData } from "./types";
+
+type SourceParams = {
+  api: ApiPromise;
+  chain: Text;
+  chainId: U32;
+};
 
 class Source {
   private api: ApiPromise;
   private chain: Text;
+  private chainId: U32;
 
-  constructor({ api, chain }: { api: ApiPromise; chain: Text }) {
+  constructor({ api, chain, chainId }: SourceParams) {
     this.api = api;
     this.chain = chain;
+    this.chainId = chainId;
   }
 
   private subscribeHeads = (): Observable<Header> =>
     this.api.rx.rpc.chain.subscribeFinalizedHeads();
 
-  private getBlock = (hash: Hash): Promise<SignedBlock> =>
-    this.api.rpc.chain.getBlock(hash);
+  private getBlock = (hash: Hash): Promise<string> =>
+    this.api.rpc.chain.getBlock(hash).then((block) => block.toString());
 
-  private getBlockByHeader = async ({ hash }: Header): Promise<string> => {
+  private getBlockByHeader = async ({ hash }: Header): Promise<TxData> => {
     const block = await this.getBlock(hash);
-    // TODO: should include size of headers?
-    // TODO: what is the size limit?
-    // TODO: check size - if too big reject
     const size = Buffer.byteLength(JSON.stringify(block));
 
     console.log(`Chain ${this.chain}: Finalized block hash: ${hash}`);
     console.log(`Chain ${this.chain}: Finalized block size: ${size / 1024} Kb`);
 
-    // TODO: clarify how we identify chains
-    return JSON.stringify({ ...block.toJSON(), chain: this.chain });
+    return { block, chainId: this.chainId };
   };
 
-  subscribeBlocks = (): Observable<string> =>
+  subscribeBlocks = (): Observable<TxData> =>
     this.subscribeHeads().pipe(concatMap(this.getBlockByHeader));
 }
 
