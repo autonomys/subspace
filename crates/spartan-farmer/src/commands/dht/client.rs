29c41b7328517856ca6fbbd5e0c647e1699ad1ab
Tejas Sanap <sanap.tejas@gmail.com>
2021-09-19 00:55:41 +0530

    Create a seperate method to create `Client` API and `EventLoop` objects

--- crates/spartan-farmer/src/commands/dht/client.rs
+++ crates/spartan-farmer/src/commands/dht/client.rs
@@ -14,18 +14,24 @@ pub struct Client {
     client_tx: Sender<ClientEvent>,
 }
 
+pub async fn dht_listener() -> (Client, EventLoop) {
+    let (network_tx, network_rx) = channel(10);
+    let (client_tx, client_rx) = channel(10);
+
+    return (
+        Client::new(network_rx, client_tx),
+        EventLoop::new(create_swarm().await, client_rx, network_tx),
+    );
+}
+
 impl Client {
     /// This method will construct a new Swarm and eventloop.
-    pub async fn new() -> (Self, EventLoop) {
-        let (network_tx, network_rx) = channel(10);
-        let (client_tx, client_rx) = channel(10);
-
-        return (
-            Client {
-                network_rx,
-                client_tx,
-            },
-            EventLoop::new(create_swarm().await, client_rx, network_tx)
-        );
+    pub fn new(network_rx: Receiver<ComposedEvent>, client_tx: Sender<ClientEvent>) -> Self {
+        Client {
+            network_rx,
+            client_tx,
+        }
+    }
+    pub fn start_listening(&mut self) {
     }
 }
