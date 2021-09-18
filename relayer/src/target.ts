061bc7f17db43fb7ba1a4d75a1f37626783b9628
Serge <isserge@protonmail.com>
2021-09-18 15:48:57 +0400

    relayer: refactoring Target to process multiple blocks per subscription event

--- relayer/src/target.ts
+++ relayer/src/target.ts
@@ -14,9 +14,11 @@ class Target {
   constructor({ api, signer }: { api: ApiPromise; signer: AddressOrPair }) {
     this.api = api;
     this.signer = signer;
+    this.processBlocks = this.processBlocks.bind(this);
+    this.sendBlockTx = this.sendBlockTx.bind(this);
   }
 
-  private sendBlockTx = ({ block, chainId }: TxData): Promise<Hash> => {
+  private sendBlockTx({ block, chainId }: TxData): Promise<Hash> {
     return (
       this.api.tx.feeds
         .put(block, chainId)
@@ -25,17 +27,26 @@ class Target {
         // https://polkadot.js.org/docs/api/cookbook/tx/#how-do-i-take-the-pending-tx-pool-into-account-in-my-nonce
         .signAndSend(this.signer, { nonce: -1 })
     );
-  };
+  }
 
-  processBlocks = (subscriptions: Observable<TxData>[]): Observable<void> => {
+  private processBlocks(blocks: TxData[]): Promise<Hash[]> {
+    return Promise.all(blocks.map(this.sendBlockTx));
+  }
+
+  private logTxs(txHashs: Hash[]) {
+    // TODO: clarify if we need to know which tx corresponds to which chain
+    txHashs.forEach((txHash) => console.log(`Transaction sent: ${txHash}`));
+  }
+
+  processSubscriptions(
+    subscriptions: Observable<TxData[]>[]
+  ): Observable<void> {
     return merge(...subscriptions).pipe(
-      concatMap(this.sendBlockTx),
-      map((txHash) => {
-        // TODO: clarify if we need to know which tx corresponds to which chain
-        console.log(`Transaction sent: ${txHash}`);
-      })
+      // subscriptions emit array of blocks - relay block and parablocks
+      concatMap(this.processBlocks),
+      map(this.logTxs)
     );
-  };
+  }
 }
 
 export default Target;
