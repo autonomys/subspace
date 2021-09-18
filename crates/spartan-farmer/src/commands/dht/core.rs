06b596a453e7e23c5bcd5afeac10773909aeb9ae
Tejas Sanap <sanap.tejas@gmail.com>
2021-09-19 00:33:24 +0530

    Add method to create swarm for farmer.

--- crates/spartan-farmer/src/commands/dht/core.rs
+++ crates/spartan-farmer/src/commands/dht/core.rs
@@ -16,3 +16,29 @@ impl From<KademliaEvent> for ComposedEvent {
         ComposedEvent::Kademlia(event)
     }
 }
+
+pub async fn create_swarm() -> Swarm<ComposedBehaviour> {
+    // Generate IDs.
+    let key = identity::Keypair::generate_ed25519();
+    let peerid = PeerId::from_public_key(key.public());
+
+    // Generate NOISE authentication keys.
+    let auth_keys = Keypair::<X25519Spec>::new().into_authentic(&key).unwrap();
+
+    // Create secure-TCP transport that uses tokio under the hood.
+    let transport = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    let behaviour = ComposedBehaviour {
+        kademlia: Kademlia::new(peerid.clone(), MemoryStore::new(peerid.clone())),
+    };
+
+    SwarmBuilder::new(transport, behaviour, peerid.clone())
+        .executor(Box::new(|fut| {
+            tokio::spawn(fut);
+        }))
+        .build()
+}
