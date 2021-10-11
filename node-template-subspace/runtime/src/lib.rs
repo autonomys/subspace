756468f2ecefd30c0c227846f230e24e66ad531d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-10-11 15:02:50 +0300

    Add `GPL-3.0-or-later` license for Node and its runtime

--- node-template-subspace/runtime/src/lib.rs
+++ node-template-subspace/runtime/src/lib.rs
@@ -1,4 +1,19 @@
-#![feature(int_log)]
+// Copyright (C) 2021 Subspace Labs, Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
