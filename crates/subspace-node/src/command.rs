ff9c6f950e9d04fba4cc7b292190e00421336d31
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-11-22 19:08:29 +0800

    Add some modifications for cirrus development (#143)
    * Separate out subspace-primitives from subspace-runtime
    
    * Introduce `NewFull` struct in subspace-node
    
    Also expose the `service` module in public.
    
    * Make subspace-node a library

--- crates/subspace-node/src/command.rs
+++ crates/subspace-node/src/command.rs
@@ -147,9 +147,11 @@ pub fn run() -> sc_cli::Result<()> {
             }
         }
         None => {
-            let runner = cli.create_runner(&cli.run)?;
+            let runner = cli.create_runner(&cli.run.base)?;
             runner.run_node_until_exit(|config| async move {
-                service::new_full(config).map_err(sc_cli::Error::Service)
+                service::new_full(config)
+                    .map(|full| full.task_manager)
+                    .map_err(sc_cli::Error::Service)
             })
         }
     }
