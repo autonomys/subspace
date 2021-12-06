5186554521221193815e80da0bd61c11cce7fa84
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-12-06 13:04:49 +0800

    Remove polkadot deps from overseer-gen

--- polkadot/node/overseer/overseer-gen/src/lib.rs
+++ polkadot/node/overseer/overseer-gen/src/lib.rs
@@ -65,7 +65,7 @@ pub use polkadot_overseer_gen_proc_macro::overlord;
 #[doc(hidden)]
 pub use metered;
 #[doc(hidden)]
-pub use polkadot_node_primitives::SpawnNamed;
+pub use sp_core::traits::SpawnNamed;
 #[doc(hidden)]
 pub use tracing;
 
@@ -93,8 +93,6 @@ pub use std::time::Duration;
 
 #[doc(hidden)]
 pub use futures_timer::Delay;
-
-pub use polkadot_node_network_protocol::WrongVariant;
 
 use std::fmt;
 
