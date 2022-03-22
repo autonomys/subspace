58446cdf45ebd50d5692896586d57c3cf9a5d54c
i1i1 <vanyarybin1@live.ru>
2022-03-22 15:58:26 +0300

    Remove test for commitments and add test for commitments removal

--- crates/subspace-farmer/src/commitments/tests.rs
+++ crates/subspace-farmer/src/commitments/tests.rs
@@ -1,9 +1,9 @@
-use crate::plot::{xor_distance, Plot};
-use crate::{commitments::Commitments, plot::PieceOffset};
+use crate::commitments::Commitments;
+use crate::plot::Plot;
 use rand::prelude::*;
 use rand::rngs::StdRng;
-use std::{collections::BTreeMap, sync::Arc};
-use subspace_core_primitives::{FlatPieces, PieceIndex, Salt, Tag, PIECE_SIZE};
+use std::sync::Arc;
+use subspace_core_primitives::{FlatPieces, Salt, Tag, PIECE_SIZE};
 use tempfile::TempDir;
 
 fn init() {
@@ -112,76 +112,30 @@ async fn find_by_tag() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn partial_replica_commitments() {
+async fn remove_commitments() {
     init();
     let base_directory = TempDir::new().unwrap();
+
+    let pieces: FlatPieces = vec![9u8; 4096].try_into().unwrap();
     let salt: Salt = [1u8; 8];
+    let correct_tag: Tag = [23, 245, 162, 52, 107, 135, 192, 210];
+    let solution_range = u64::from_be_bytes([0xff_u8, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+    let index = 0;
 
-    let max_pieces = 64;
-
-    let address = rand::random::<[u8; 32]>().into();
-    let plot = Plot::open_or_create(
-        &base_directory,
-        address,
-        Some(max_pieces * PIECE_SIZE as u64),
-    )
-    .unwrap();
+    let plot = Plot::open_or_create(&base_directory, [0; 32].into(), None).unwrap();
     let commitments = Commitments::new(base_directory.path().join("commitments")).unwrap();
-
-    let mut pieces: FlatPieces = vec![0u8; 1024 * PIECE_SIZE].try_into().unwrap();
-    // Generate deterministic pieces, such that we don't have random errors in CI
-    StdRng::seed_from_u64(0).fill(pieces.as_mut());
-
-    let mut piece_indexes = (0..1024).collect::<Vec<_>>();
-    piece_indexes.sort_by_key(|i| xor_distance((*i).into(), address));
-
-    let expected_commitments = piece_indexes[..max_pieces as usize]
-        .iter()
-        .map(|&i| (i, pieces.chunks(PIECE_SIZE).nth(i as usize).unwrap()))
-        .map(|(i, piece)| (i, *<&[u8; PIECE_SIZE]>::try_from(piece).unwrap()))
-        .map(|(i, piece)| (subspace_solving::create_tag(piece, salt), i as PieceIndex))
-        .collect::<BTreeMap<Tag, PieceIndex>>();
-
-    plot.write_many(Arc::new(pieces), 0).unwrap();
-    assert_eq!(plot.piece_count(), max_pieces);
-
+    plot.write_many(Arc::new(pieces), index).unwrap();
     commitments.create(salt, plot.clone()).unwrap();
 
-    let get_solutions = |target, solution_range, salt| {
-        commitments
-            .find_by_range_many::<BTreeMap<_, PieceOffset>>(target, solution_range, salt)
-            .unwrap()
-            .into_iter()
-            .map(|(tag, offset)| (tag, plot.read_piece_with_index(offset).unwrap().1))
-    };
+    let (_, offset) = commitments
+        .find_by_range(correct_tag, solution_range, salt)
+        .unwrap();
 
-    // Check all commitments
-    let target = rand::random();
-    let all_commitmens = get_solutions(target, u64::MAX, salt).collect::<BTreeMap<_, _>>();
-    assert_eq!(all_commitmens, expected_commitments);
+    commitments
+        .remove_pieces(vec![plot.read_piece_with_index(offset).unwrap().0])
+        .unwrap();
 
-    // make random queries
-    for _ in 0..10 {
-        let target = rand::random();
-        let range = rand::random();
-
-        let (lower, lower_overflow) = u64::from_be_bytes(target).overflowing_sub(range / 2);
-        let (upper, upper_overflow) = u64::from_be_bytes(target).overflowing_add(range / 2);
-        let expected = if lower_overflow || upper_overflow {
-            expected_commitments
-                .range(..upper.to_be_bytes())
-                .chain(expected_commitments.range(lower.to_be_bytes()..))
-                .map(|(&key, &val)| (key, val))
-                .collect()
-        } else {
-            expected_commitments
-                .range(lower.to_be_bytes()..upper.to_be_bytes())
-                .map(|(&key, &val)| (key, val))
-                .collect::<BTreeMap<_, _>>()
-        };
-
-        let got = get_solutions(target, range, salt).collect::<BTreeMap<_, _>>();
-
-        assert_eq!(expected, got);
-    }
+    assert!(commitments
+        .find_by_range(correct_tag, solution_range, salt)
+        .is_none());
 }
