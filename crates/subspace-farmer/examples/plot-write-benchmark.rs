c51f5000cfe45699b0c43d074c95f4b3d2cce09c
i1i1 <vanyarybin1@live.ru>
2022-03-24 16:52:20 +0300

    Change arguments from megabytes to pieces

--- crates/subspace-farmer/examples/plot-write-benchmark.rs
+++ crates/subspace-farmer/examples/plot-write-benchmark.rs
@@ -7,32 +7,27 @@ use subspace_farmer::Plot;
 
 #[derive(Debug, Parser)]
 struct Args {
-    /// In megabytes
-    #[clap(short, long, default_value = "8")]
-    buffer_size: u64,
-    /// In megabytes
-    #[clap(short, long, default_value = "1024")]
-    write_size: u64,
+    /// Number of pieces in a batch
+    #[clap(short, long, default_value = "4096")]
+    batch_size: u64,
+    /// Number of pieces to write
+    #[clap(short, long, default_value = "262144")]
+    piece_count: u64,
 
     #[clap(value_hint = ValueHint::DirPath)]
     base_directory: PathBuf,
 }
 
-// $ dd if=/dev/zero of=/home/i1i1/tmp/zeros bs=100M count=80
-// 8388608000 bytes (8.4 GB, 7.8 GiB) copied, 25.4853 s, 329 MB/s
-//
-// Writing 8000M to disk took 5.893584945s. Speed is around 1357.4081097765531 M/s
 #[tokio::main]
 async fn main() {
     let Args {
-        buffer_size,
-        write_size,
+        batch_size,
+        piece_count,
         base_directory,
     } = Args::parse();
 
-    let npieces = buffer_size * 1024 * 1024 / PIECE_SIZE as u64;
-    let mut pieces = Vec::with_capacity(npieces as usize * PIECE_SIZE);
-    pieces.resize(npieces as usize * PIECE_SIZE, 0u8);
+    let mut pieces = Vec::with_capacity(batch_size as usize * PIECE_SIZE);
+    pieces.resize(batch_size as usize * PIECE_SIZE, 0u8);
     rand::thread_rng().fill(&mut pieces[..]);
     let pieces = Arc::new(pieces.try_into().unwrap());
 
@@ -40,19 +35,13 @@ async fn main() {
 
     let start = std::time::Instant::now();
 
-    for index in (0..write_size / buffer_size).map(|i| i * npieces) {
+    for index in (0..piece_count / batch_size).map(|i| i * batch_size) {
         plot.write_many(Arc::clone(&pieces), index as u64).unwrap();
     }
     drop(plot);
 
-    // Just in case
-    std::process::Command::new("sync")
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-
     let took = start.elapsed();
+    let write_size = piece_count * PIECE_SIZE as u64 / 1024 / 1024;
     eprintln!(
         "Writing {write_size}M to disk took {took:?}. Speed is around {:.2} M/s",
         write_size as f64 / took.as_secs_f64()
