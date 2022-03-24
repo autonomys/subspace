5ee3741be17f784a81eb6ebe8bc199480287da3f
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-03-24 19:21:53 +0200

    Move build script from `subspace-consensus-runtime` to `subspace-runtime`

--- crates/subspace-consensus-runtime/src/lib.rs
+++ crates/subspace-consensus-runtime/src/lib.rs
@@ -19,10 +19,6 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
-// Make the WASM binary available.
-#[cfg(feature = "std")]
-include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
-
 use codec::{Compact, CompactLen, Decode, Encode};
 use core::time::Duration;
 use frame_support::traits::{
--- crates/subspace-runtime/src/lib.rs
+++ crates/subspace-runtime/src/lib.rs
@@ -1,1 +1,9 @@
+//! Subspace runtime
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// Make the WASM binary available.
+#[cfg(feature = "std")]
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+
 pub use subspace_consensus_runtime::*;
