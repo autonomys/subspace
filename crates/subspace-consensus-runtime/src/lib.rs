58273b7a882a2e82c4961e9b6e1a314bc0a9c1fe
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-03-27 21:29:45 +0800

    Reset spec_version to 1
    Should be fine after the new Substrate metadata was released.

--- crates/subspace-consensus-runtime/src/lib.rs
+++ crates/subspace-consensus-runtime/src/lib.rs
@@ -71,12 +71,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subspace"),
     impl_name: create_runtime_str!("subspace"),
     authoring_version: 1,
-    // The version of the runtime specification. A full node will not attempt to use its native
-    //   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-    //   `spec_version`, and `authoring_version` are the same between Wasm and native.
-    // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
-    //   the compatible custom types.
-    spec_version: 100,
+    spec_version: 1,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
--- cumulus/parachain-template/runtime/src/lib.rs
+++ cumulus/parachain-template/runtime/src/lib.rs
@@ -136,7 +136,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("subspace"),
 	impl_name: create_runtime_str!("subspace"),
 	authoring_version: 1,
-	spec_version: 100,
+	spec_version: 1,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
