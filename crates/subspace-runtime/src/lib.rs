2067211b25cbe075d819e3601733942a34be2906
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-04-18 19:32:36 +0200

    pass thr right type to the associated type

--- crates/subspace-runtime/src/lib.rs
+++ crates/subspace-runtime/src/lib.rs
@@ -273,7 +273,7 @@ impl pallet_subspace::Config for Runtime {
     type RecordSize = ConstU32<RECORD_SIZE>;
     type MaxPlotSize = ConstU64<MAX_PLOT_SIZE>;
     type RecordedHistorySegmentSize = ConstU32<RECORDED_HISTORY_SEGMENT_SIZE>;
-    type ShouldAdjustSolutionRange = ();
+    type ShouldAdjustSolutionRange = ShouldAdjustSolutionRange;
     type GlobalRandomnessIntervalTrigger = pallet_subspace::NormalGlobalRandomnessInterval;
     type EraChangeTrigger = pallet_subspace::NormalEraChange;
     type EonChangeTrigger = pallet_subspace::NormalEonChange;
