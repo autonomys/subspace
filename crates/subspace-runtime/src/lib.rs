7a4af9f38d3ec684a6d29d3bd576841a03b027de
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-05-18 16:27:39 +0300

    Reset runtime versions

--- crates/subspace-runtime/src/lib.rs
+++ crates/subspace-runtime/src/lib.rs
@@ -87,17 +87,12 @@ sp_runtime::impl_opaque_keys! {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subspace"),
     impl_name: create_runtime_str!("subspace"),
-    authoring_version: 1,
-    // The version of the runtime specification. A full node will not attempt to use its native
-    //   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-    //   `spec_version`, and `authoring_version` are the same between Wasm and native.
-    // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
-    //   the compatible custom types.
-    spec_version: 103,
-    impl_version: 1,
+    authoring_version: 0,
+    spec_version: 0,
+    impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 1,
-    state_version: 1,
+    transaction_version: 0,
+    state_version: 0,
 };
 
 /// The version information used to identify this runtime when compiled natively.
