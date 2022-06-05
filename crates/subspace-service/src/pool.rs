92f87d61b404c101a76104de6a468a9e54031a90
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-06-05 16:14:41 +0800

    Inline `sc_transaction_pool::BasicPool::new_full`
    `BasicPool` and `FullChainApi` will be replaced with `BasicPoolWrapper`
    and `FullChainApiWrapper` later.

--- crates/subspace-service/src/pool.rs
+++ crates/subspace-service/src/pool.rs
@@ -1,7 +1,7 @@
 use sc_client_api::blockchain::HeaderBackend;
 use sc_client_api::{BlockBackend, ExecutorProvider, UsageProvider};
 use sc_service::Configuration;
-use sc_transaction_pool::{BasicPool, FullChainApi};
+use sc_transaction_pool::{BasicPool, FullChainApi, RevalidationType};
 use sp_api::ProvideRuntimeApi;
 use sp_core::traits::SpawnEssentialNamed;
 use sp_runtime::traits::{Block as BlockT, BlockIdTo};
@@ -26,11 +26,22 @@ where
         + 'static,
     Client::Api: TaggedTransactionQueue<Block>,
 {
-    sc_transaction_pool::BasicPool::new_full(
+    let prometheus = config.prometheus_registry();
+    let pool_api = Arc::new(FullChainApi::new(client.clone(), prometheus, &spawner));
+    let pool = Arc::new(BasicPool::with_revalidation_type(
         config.transaction_pool.clone(),
         config.role.is_authority().into(),
-        config.prometheus_registry(),
+        pool_api,
+        prometheus,
+        RevalidationType::Full,
         spawner,
-        client,
-    )
+        client.usage_info().chain.best_number,
+    ));
+
+    // make transaction pool available for off-chain runtime calls.
+    client
+        .execution_extensions()
+        .register_transaction_pool(&pool);
+
+    pool
 }
