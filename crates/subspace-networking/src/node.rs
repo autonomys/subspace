39847aba2cbdff6636255e4cafd3747795e3f85c
Shamil Gadelshin <shamilgadelshin@gmail.com>
2022-06-06 15:43:43 +0300

    networking: Add `get-pieces-by-range` method smoke test.

--- crates/subspace-networking/src/node.rs
+++ crates/subspace-networking/src/node.rs
@@ -349,3 +349,77 @@ impl Node {
             .map_err(|_| SendPiecesByRangeRequestError::IncorrectResponseFormat)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{Config, PiecesByRangeResponse};
+    use futures::channel::mpsc;
+    use futures::StreamExt;
+    use libp2p::multiaddr::Protocol;
+    use std::sync::Arc;
+    use std::time::Duration;
+    use subspace_core_primitives::{crypto, Piece, PieceIndexHash};
+
+    #[tokio::test]
+    async fn get_pieces_by_range_protocol_smoke() {
+        let piece = Piece::default();
+        let expected_piece = piece.clone();
+
+        let config_1 = Config {
+            listen_on: vec!["/ip4/0.0.0.0/tcp/0".parse().unwrap()],
+            allow_non_globals_in_dht: true,
+            pieces_by_range_request_handler: Arc::new(move |_| {
+                Some(PiecesByRangeResponse {
+                    pieces: vec![piece.clone()],
+                    next_piece_hash_index: None,
+                })
+            }),
+            ..Config::with_generated_keypair()
+        };
+        let (node_1, mut node_runner_1) = crate::create(config_1).await.unwrap();
+
+        let (node_1_addresses_sender, mut node_1_addresses_receiver) = mpsc::unbounded();
+        node_1
+            .on_new_listener(Arc::new(move |address| {
+                node_1_addresses_sender
+                    .unbounded_send(address.clone())
+                    .unwrap();
+            }))
+            .detach();
+
+        tokio::spawn(async move {
+            node_runner_1.run().await;
+        });
+
+        let config_2 = Config {
+            bootstrap_nodes: vec![node_1_addresses_receiver
+                .next()
+                .await
+                .unwrap()
+                .with(Protocol::P2p(node_1.id().into()))],
+            listen_on: vec!["/ip4/0.0.0.0/tcp/0".parse().unwrap()],
+            allow_non_globals_in_dht: true,
+            ..Config::with_generated_keypair()
+        };
+
+        let (node_2, mut node_runner_2) = crate::create(config_2).await.unwrap();
+        tokio::spawn(async move {
+            node_runner_2.run().await;
+        });
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        let hashed_peer_id = PieceIndexHash(crypto::sha256_hash(&node_1.id().to_bytes()));
+
+        let mut stream = node_2
+            .get_pieces_by_range(hashed_peer_id, hashed_peer_id)
+            .await
+            .unwrap();
+
+        let result = stream.next().await;
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        assert_eq!(result.unwrap(), expected_piece);
+    }
+}
