fedd7893e5c9b1a8ae0f88e88ebd9abfdf2eac02
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-07-04 13:38:48 +0300

    Move `get_plot_sizes` function into utils

--- crates/subspace-farmer/src/legacy_multi_plots_farm.rs
+++ crates/subspace-farmer/src/legacy_multi_plots_farm.rs
@@ -3,6 +3,7 @@ use crate::object_mappings::ObjectMappings;
 use crate::rpc_client::RpcClient;
 use crate::single_disk_farm::SingleDiskFarmPieceGetter;
 use crate::single_plot_farm::{PlotFactory, SinglePlotFarm, SinglePlotFarmOptions};
+use crate::utils::get_plot_sizes;
 use anyhow::anyhow;
 use futures::stream::{FuturesUnordered, StreamExt};
 use parking_lot::Mutex;
@@ -13,26 +14,6 @@ use subspace_core_primitives::{PublicKey, PIECE_SIZE};
 use subspace_networking::libp2p::Multiaddr;
 use tokio::runtime::Handle;
 use tracing::error;
-
-fn get_plot_sizes(allocated_space: u64, max_plot_size: u64) -> Vec<u64> {
-    // TODO: we need to remember plot size in order to prune unused plots in future if plot size is
-    //  less than it was specified before.
-    // TODO: Piece count should account for database overhead of various additional databases.
-    //  For now assume 92% will go for plot itself
-    let usable_space_for_plots = allocated_space * 92 / 100;
-
-    let plot_sizes =
-        std::iter::repeat(max_plot_size).take((usable_space_for_plots / max_plot_size) as usize);
-    if usable_space_for_plots / max_plot_size == 0
-        || usable_space_for_plots % max_plot_size > max_plot_size / 2
-    {
-        plot_sizes
-            .chain(std::iter::once(usable_space_for_plots % max_plot_size))
-            .collect::<Vec<_>>()
-    } else {
-        plot_sizes.collect()
-    }
-}
 
 /// Options for `MultiFarming` creation
 pub struct Options<C> {
--- crates/subspace-farmer/src/utils.rs
+++ crates/subspace-farmer/src/utils.rs
@@ -26,3 +26,23 @@ impl<T> AbortingJoinHandle<T> {
         Self(handle)
     }
 }
+
+pub(crate) fn get_plot_sizes(allocated_space: u64, max_plot_size: u64) -> Vec<u64> {
+    // TODO: we need to remember plot size in order to prune unused plots in future if plot size is
+    //  less than it was specified before.
+    // TODO: Piece count should account for database overhead of various additional databases.
+    //  For now assume 92% will go for plot itself
+    let usable_space_for_plots = allocated_space * 92 / 100;
+
+    let plot_sizes =
+        std::iter::repeat(max_plot_size).take((usable_space_for_plots / max_plot_size) as usize);
+    if usable_space_for_plots / max_plot_size == 0
+        || usable_space_for_plots % max_plot_size > max_plot_size / 2
+    {
+        plot_sizes
+            .chain(std::iter::once(usable_space_for_plots % max_plot_size))
+            .collect::<Vec<_>>()
+    } else {
+        plot_sizes.collect()
+    }
+}
