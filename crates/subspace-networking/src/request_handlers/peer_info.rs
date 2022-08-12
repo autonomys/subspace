1c3b22b8394be127676f2bdc596d99dd36e19476
Shamil Gadelshin <shamilgadelshin@gmail.com>
2022-08-12 21:10:39 +0300

    networking: Introduce PeerInfo request-response protocol.

---
+++ crates/subspace-networking/src/request_handlers/peer_info.rs
@@ -0,0 +1,40 @@
+use crate::{GenericRequest, GenericRequestHandler};
+use parity_scale_codec::{Decode, Encode};
+
+/// Peer-info protocol request.
+#[derive(Debug, Clone, Eq, PartialEq, Encode, Decode)]
+pub struct PeerInfoRequest;
+
+/// Defines peer synchronization status.
+#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
+pub enum PeerSyncStatus {
+    /// Synchronization is not supported for this peer.
+    NotSupported,
+    /// Peer is ready to provide data for the synchronization.
+    Ready,
+    /// Peer is synchronizing itself.
+    Syncing,
+}
+
+/// Defines peer current state.
+#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
+pub struct PeerInfo {
+    /// Synchronization status.
+    pub status: PeerSyncStatus,
+}
+
+impl GenericRequest for PeerInfoRequest {
+    const PROTOCOL_NAME: &'static str = "/subspace/sync/peer-info/0.1.0";
+    const LOG_TARGET: &'static str = "peer-info-request-response-handler";
+    type Response = PeerInfoResponse;
+}
+
+/// Peer-info protocol response.
+#[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
+pub struct PeerInfoResponse {
+    /// Returned data.
+    pub peer_info: PeerInfo,
+}
+
+/// Create a new peer-info request handler.
+pub type PeerInfoRequestHandler = GenericRequestHandler<PeerInfoRequest>;
