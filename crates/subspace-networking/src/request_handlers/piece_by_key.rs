15ad9a813230f6f1910683e5495b7b5db127b4b2
Shamil Gadelshin <shamilgadelshin@gmail.com>
2022-10-07 13:09:51 +0400

    networking: Modify piece-by-key protocol.

--- crates/subspace-networking/src/request_handlers/piece_by_key.rs
+++ crates/subspace-networking/src/request_handlers/piece_by_key.rs
@@ -5,13 +5,21 @@
 
 use crate::request_handlers::generic_request_handler::{GenericRequest, GenericRequestHandler};
 use parity_scale_codec::{Decode, Encode};
-use subspace_core_primitives::{Piece, PieceIndexHash};
+use subspace_core_primitives::{Piece, PieceIndex, PieceIndexHash};
+
+//TODO: rename all module names
+#[derive(Debug, Clone, Eq, PartialEq, Encode, Decode)]
+pub enum PieceKey {
+    PieceIndex(PieceIndex),
+    PieceIndexHash(PieceIndexHash),
+}
 
 /// Piece-by-hash protocol request.
 #[derive(Debug, Clone, Eq, PartialEq, Encode, Decode)]
 pub struct PieceByHashRequest {
+    //TODO: rename
     /// Piece index hash
-    pub piece_index_hash: PieceIndexHash,
+    pub key: PieceKey,
 }
 
 impl GenericRequest for PieceByHashRequest {
