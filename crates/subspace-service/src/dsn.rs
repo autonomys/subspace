524f59cf7a4b5796e1febece045253ca9c9c68dd
Shamil Gadelshin <shamilgadelshin@gmail.com>
2022-10-07 13:08:19 +0400

    node: Refactor dsn config.

--- crates/subspace-service/src/dsn.rs
+++ crates/subspace-service/src/dsn.rs
@@ -3,22 +3,55 @@ use futures::{FutureExt, StreamExt};
 use sc_consensus_subspace::{ArchivedSegmentNotification, SubspaceLink};
 use sp_core::traits::SpawnEssentialNamed;
 use sp_runtime::traits::Block as BlockT;
-use subspace_core_primitives::{PieceIndexHash, PIECES_IN_SEGMENT};
-use subspace_networking::{CreationError, ToMultihash};
+use std::sync::Arc;
+use subspace_core_primitives::{Piece, PieceIndex, PieceIndexHash, PIECES_IN_SEGMENT};
+use subspace_networking::libp2p::{identity, Multiaddr};
+use subspace_networking::{
+    CreationError, PieceByHashRequestHandler, PieceByHashResponse, PieceKey, ToMultihash,
+};
 use tracing::{error, info, trace};
+
+pub type PieceGetter = Arc<dyn (Fn(&PieceIndex) -> Option<Piece>) + Send + Sync + 'static>;
+
+/// DSN configuration parameters.
+#[derive(Clone, Debug)]
+pub struct DsnConfig {
+    /// DSN 'listen-on' multi-address
+    pub dsn_listen_on: Vec<Multiaddr>,
+
+    /// Identity keypair of a node used for authenticated connections.
+    pub keypair: identity::Keypair,
+}
 
 /// Start an archiver that will listen for archived segments and send it to DSN network using
 /// pub-sub protocol.
 pub async fn start_dsn_node<Block, Spawner>(
     subspace_link: &SubspaceLink<Block>,
-    networking_config: subspace_networking::Config,
+    dsn_config: DsnConfig,
     spawner: Spawner,
+    piece_getter: PieceGetter,
 ) -> Result<(), CreationError>
 where
     Block: BlockT,
     Spawner: SpawnEssentialNamed,
 {
     trace!(target: "dsn", "Subspace networking starting.");
+
+    let networking_config = subspace_networking::Config {
+        keypair: dsn_config.keypair,
+        listen_on: dsn_config.dsn_listen_on,
+        request_response_protocols: vec![PieceByHashRequestHandler::create(move |req| {
+            let result = if let PieceKey::PieceIndex(idx) = req.key {
+                piece_getter(&idx)
+            } else {
+                // TODO
+                None
+            };
+
+            Some(PieceByHashResponse { piece: result })
+        })],
+        ..subspace_networking::Config::with_generated_keypair()
+    };
 
     let (node, mut node_runner) = subspace_networking::create(networking_config).await?;
 
