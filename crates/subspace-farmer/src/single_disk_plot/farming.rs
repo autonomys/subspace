6e1aaf932d1f88ecc9fdb29b7cc09e46dcd28750
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-10-21 14:43:46 +0300

    Reorder functions, no other changes

--- crates/subspace-farmer/src/single_disk_plot/farming.rs
+++ crates/subspace-farmer/src/single_disk_plot/farming.rs
@@ -35,72 +35,6 @@ pub struct EligibleSector {
     pub audit_piece_offset: u64,
 }
 
-/// Audit a single sector
-///
-/// Note: auditing expects cursor to be set to the beginning of the sector and will move the cursor
-/// during its operation. Make sure to return it back to the beginning of the sector if necessary.
-pub fn audit_sector<S>(
-    public_key: &PublicKey,
-    sector_index: u64,
-    farmer_protocol_info: &FarmerProtocolInfo,
-    global_challenge: &Blake2b256Hash,
-    solution_range: SolutionRange,
-    mut sector: S,
-) -> Result<Option<EligibleSector>, FarmingError>
-where
-    S: io::Read + io::Seek,
-{
-    let sector_id = SectorId::new(public_key, sector_index);
-    let chunks_in_sector = u64::from(farmer_protocol_info.record_size.get()) * u64::from(u8::BITS)
-        / u64::from(farmer_protocol_info.space_l.get());
-
-    let local_challenge = sector_id.derive_local_challenge(global_challenge);
-    let audit_index: u64 = local_challenge % chunks_in_sector;
-    let audit_piece_offset = (audit_index / u64::from(u8::BITS)) / PIECE_SIZE as u64;
-    // Offset of the piece in sector (in bytes)
-    let audit_piece_bytes_offset = audit_piece_offset * PIECE_SIZE as u64;
-    // Audit index (chunk) within corresponding piece
-    let audit_index_within_piece = audit_index - audit_piece_bytes_offset * u64::from(u8::BITS);
-    let mut piece = Piece::default();
-    sector.seek(SeekFrom::Current(audit_piece_bytes_offset as i64))?;
-    sector.read_exact(&mut piece)?;
-
-    // TODO: We are skipping witness part of the piece or else it is not
-    //  decodable
-    let maybe_chunk = piece[..farmer_protocol_info.record_size.get() as usize]
-        .view_bits()
-        .chunks_exact(farmer_protocol_info.space_l.get() as usize)
-        .nth(audit_index_within_piece as usize);
-
-    let chunk = match maybe_chunk {
-        Some(chunk) => Chunk::from(chunk),
-        None => {
-            // TODO: Record size is not multiple of `space_l`, last bits
-            //  were not encoded and should not be used for solving
-            return Ok(None);
-        }
-    };
-
-    // TODO: This just have 20 bits of entropy as input, should we add
-    //  something else?
-    let expanded_chunk = chunk.expand(local_challenge);
-
-    Ok(
-        is_within_solution_range(local_challenge, expanded_chunk, solution_range).then_some(
-            EligibleSector {
-                sector_id,
-                sector_index,
-                local_challenge,
-                audit_index,
-                chunk,
-                expanded_chunk,
-                encoded_piece: piece,
-                audit_piece_offset,
-            },
-        ),
-    )
-}
-
 /// Create solution for eligible sector
 pub fn create_solution<SM>(
     keypair: &Keypair,
@@ -173,4 +107,70 @@ where
         chunk: eligible_sector.chunk,
         chunk_signature: create_chunk_signature(keypair, &eligible_sector.chunk),
     }))
+}
+
+/// Audit a single sector
+///
+/// Note: auditing expects cursor to be set to the beginning of the sector and will move the cursor
+/// during its operation. Make sure to return it back to the beginning of the sector if necessary.
+pub fn audit_sector<S>(
+    public_key: &PublicKey,
+    sector_index: u64,
+    farmer_protocol_info: &FarmerProtocolInfo,
+    global_challenge: &Blake2b256Hash,
+    solution_range: SolutionRange,
+    mut sector: S,
+) -> Result<Option<EligibleSector>, FarmingError>
+where
+    S: io::Read + io::Seek,
+{
+    let sector_id = SectorId::new(public_key, sector_index);
+    let chunks_in_sector = u64::from(farmer_protocol_info.record_size.get()) * u64::from(u8::BITS)
+        / u64::from(farmer_protocol_info.space_l.get());
+
+    let local_challenge = sector_id.derive_local_challenge(global_challenge);
+    let audit_index: u64 = local_challenge % chunks_in_sector;
+    let audit_piece_offset = (audit_index / u64::from(u8::BITS)) / PIECE_SIZE as u64;
+    // Offset of the piece in sector (in bytes)
+    let audit_piece_bytes_offset = audit_piece_offset * PIECE_SIZE as u64;
+    // Audit index (chunk) within corresponding piece
+    let audit_index_within_piece = audit_index - audit_piece_bytes_offset * u64::from(u8::BITS);
+    let mut piece = Piece::default();
+    sector.seek(SeekFrom::Current(audit_piece_bytes_offset as i64))?;
+    sector.read_exact(&mut piece)?;
+
+    // TODO: We are skipping witness part of the piece or else it is not
+    //  decodable
+    let maybe_chunk = piece[..farmer_protocol_info.record_size.get() as usize]
+        .view_bits()
+        .chunks_exact(farmer_protocol_info.space_l.get() as usize)
+        .nth(audit_index_within_piece as usize);
+
+    let chunk = match maybe_chunk {
+        Some(chunk) => Chunk::from(chunk),
+        None => {
+            // TODO: Record size is not multiple of `space_l`, last bits
+            //  were not encoded and should not be used for solving
+            return Ok(None);
+        }
+    };
+
+    // TODO: This just have 20 bits of entropy as input, should we add
+    //  something else?
+    let expanded_chunk = chunk.expand(local_challenge);
+
+    Ok(
+        is_within_solution_range(local_challenge, expanded_chunk, solution_range).then_some(
+            EligibleSector {
+                sector_id,
+                sector_index,
+                local_challenge,
+                audit_index,
+                chunk,
+                expanded_chunk,
+                encoded_piece: piece,
+                audit_piece_offset,
+            },
+        ),
+    )
 }
