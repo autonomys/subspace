1f9cfc1d4671ccebe04aa808f4a360cd9c4f0ba0
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-11-14 09:49:19 +0800

    Add a quick doc of core-domain-args in the help
    There will probably be a furture refinement on cli later, let's
    use this simple instruction as a start.

--- crates/subspace-node/src/lib.rs
+++ crates/subspace-node/src/lib.rs
@@ -188,9 +188,12 @@ pub struct Cli {
     /// Secondary chain arguments
     ///
     /// The command-line arguments provided first will be passed to the embedded primary node,
-    /// while the arguments provided after -- will be passed to the executor node.
+    /// while the arguments provided after `--` will be passed to the system domain node.
     ///
-    /// subspace-node [primarychain-args] -- [secondarychain-args]
+    /// If you want to run a core domain node, specify the core domain node arguments after
+    /// the system domain node arguments with an extra `-- --`.
+    ///
+    /// subspace-node [primarychain-args] -- [secondarychain-args] -- -- [core-domain-args]
     #[clap(raw = true)]
     pub secondary_chain_args: Vec<String>,
 }
