767e53a00e5c13c15376de0d778f4f5cf5d5d8cc
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-11-15 13:11:04 +0200

    Add currently failing test case for https://github.com/arkworks-rs/algebra/issues/516

--- crates/subspace-core-primitives/src/sector_codec/tests.rs
+++ crates/subspace-core-primitives/src/sector_codec/tests.rs
@@ -28,3 +28,32 @@ fn basic() {
 
     assert_eq!(sector, decoded);
 }
+
+// TODO: Unlock this test once https://github.com/arkworks-rs/algebra/issues/516 is resolved
+#[test]
+#[ignore]
+fn two_way_transformation_works() {
+    let rows_columns_count = 4_usize;
+    let sector_size = rows_columns_count.pow(2) * Scalar::SAFE_BYTES;
+
+    let mut scalars = {
+        let mut sector = Vec::with_capacity(sector_size / Scalar::SAFE_BYTES);
+
+        for _ in 0..sector.capacity() {
+            sector.push(Scalar::try_from(rand::random::<[u8; 31]>().as_slice()).unwrap());
+        }
+
+        sector
+    };
+
+    let codec = SectorCodec::new(sector_size).unwrap();
+
+    codec.encode(&mut scalars).unwrap();
+
+    let new_scalars = scalars
+        .iter()
+        .map(|scalar| Scalar::from(&scalar.to_bytes()))
+        .collect::<Vec<_>>();
+
+    assert_eq!(scalars, new_scalars);
+}
