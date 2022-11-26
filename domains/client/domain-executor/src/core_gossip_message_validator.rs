90f754d2327900f2f323dca6f134c307dac016dd
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-11-26 23:03:36 +0800

    Integrate GossipMessageValidator into CoreGossipMessageValidator and SystemGossipMessageValidator

--- domains/client/domain-executor/src/core_gossip_message_validator.rs
+++ domains/client/domain-executor/src/core_gossip_message_validator.rs
@@ -1,6 +1,6 @@
-use crate::fraud_proof::{find_trace_mismatch, FraudProofGenerator};
-use crate::gossip_message_validator::GossipMessageError;
-use crate::{ExecutionReceiptFor, TransactionFor, LOG_TARGET};
+use crate::fraud_proof::FraudProofGenerator;
+use crate::gossip_message_validator::{GossipMessageError, GossipMessageValidator};
+use crate::{ExecutionReceiptFor, TransactionFor};
 use domain_client_executor_gossip::{Action, GossipMessageHandler};
 use sc_client_api::{AuxStore, BlockBackend, ProofProvider};
 use sp_api::ProvideRuntimeApi;
@@ -10,7 +10,7 @@ use sp_core::H256;
 use sp_domains::fraud_proof::{BundleEquivocationProof, InvalidTransactionProof};
 use sp_domains::{Bundle, ExecutorApi, SignedBundle};
 use sp_runtime::generic::BlockId;
-use sp_runtime::traits::{Block as BlockT, HashFor, Header as HeaderT, NumberFor};
+use sp_runtime::traits::{Block as BlockT, HashFor, NumberFor};
 use sp_runtime::RuntimeAppPublic;
 use std::marker::PhantomData;
 use std::sync::Arc;
@@ -36,9 +36,8 @@ pub struct CoreGossipMessageValidator<
     // TODO: no longer used in executor, revisit this with ParachainBlockImport together.
     primary_chain_client: Arc<PClient>,
     client: Arc<Client>,
-    spawner: Box<dyn SpawnNamed + Send + Sync>,
     transaction_pool: Arc<TransactionPool>,
-    fraud_proof_generator: FraudProofGenerator<Block, PBlock, Client, Backend, E>,
+    gossip_message_validator: GossipMessageValidator<Block, PBlock, Client, PClient, Backend, E>,
     _phantom_data: PhantomData<(SBlock, SClient, Backend)>,
 }
 
@@ -63,9 +62,8 @@ where
         Self {
             primary_chain_client: self.primary_chain_client.clone(),
             client: self.client.clone(),
-            spawner: self.spawner.clone(),
             transaction_pool: self.transaction_pool.clone(),
-            fraud_proof_generator: self.fraud_proof_generator.clone(),
+            gossip_message_validator: self.gossip_message_validator.clone(),
             _phantom_data: self._phantom_data,
         }
     }
@@ -114,65 +112,18 @@ where
         transaction_pool: Arc<TransactionPool>,
         fraud_proof_generator: FraudProofGenerator<Block, PBlock, Client, Backend, E>,
     ) -> Self {
+        let gossip_message_validator = GossipMessageValidator::new(
+            primary_chain_client.clone(),
+            client.clone(),
+            spawner,
+            fraud_proof_generator,
+        );
         Self {
             primary_chain_client,
             client,
-            spawner,
             transaction_pool,
-            fraud_proof_generator,
+            gossip_message_validator,
             _phantom_data: PhantomData::default(),
-        }
-    }
-
-    /// The background is that a receipt received from the network points to a future block
-    /// from the local view, so we need to wait for the receipt for the block at the same
-    /// height to be produced locally in order to check the validity of the external receipt.
-    async fn wait_for_local_future_receipt(
-        &self,
-        secondary_block_hash: Block::Hash,
-        secondary_block_number: <Block::Header as HeaderT>::Number,
-        tx: crossbeam::channel::Sender<
-            sp_blockchain::Result<ExecutionReceiptFor<PBlock, Block::Hash>>,
-        >,
-    ) -> Result<(), GossipMessageError> {
-        loop {
-            match crate::aux_schema::load_execution_receipt(&*self.client, secondary_block_hash) {
-                Ok(Some(local_receipt)) => {
-                    return tx
-                        .send(Ok(local_receipt))
-                        .map_err(|_| GossipMessageError::SendError)
-                }
-                Ok(None) => {
-                    // TODO: test how this works under the primary forks.
-                    //       ref https://github.com/subspace/subspace/pull/250#discussion_r804247551
-                    //
-                    // Whether or not the best execution chain number on primary chain has been
-                    // updated, the local client has proceeded to a higher block, that means the receipt
-                    // of `block_hash` received from the network does not match the local one,
-                    // we should just send back the local receipt at the same height.
-                    if self.client.info().best_number >= secondary_block_number {
-                        let local_block_hash = self
-                            .client
-                            .expect_block_hash_from_id(&BlockId::Number(secondary_block_number))?;
-                        let local_receipt_result = crate::aux_schema::load_execution_receipt(
-                            &*self.client,
-                            local_block_hash,
-                        )?
-                        .ok_or_else(|| {
-                            sp_blockchain::Error::Backend(format!(
-                                "Execution receipt not found for {:?}",
-                                local_block_hash
-                            ))
-                        });
-                        return tx
-                            .send(local_receipt_result)
-                            .map_err(|_| GossipMessageError::SendError);
-                    } else {
-                        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-                    }
-                }
-                Err(e) => return tx.send(Err(e)).map_err(|_| GossipMessageError::SendError),
-            }
         }
     }
 
@@ -181,11 +132,6 @@ where
         signed_bundle_hash: H256,
         execution_receipt: &ExecutionReceiptFor<PBlock, Block::Hash>,
     ) -> Result<(), GossipMessageError> {
-        let primary_number: BlockNumber = execution_receipt
-            .primary_number
-            .try_into()
-            .unwrap_or_else(|_| panic!("Primary number must fit into u32; qed"));
-
         let head_receipt_number = self
             .primary_chain_client
             .runtime_api()
@@ -194,55 +140,14 @@ where
             .try_into()
             .unwrap_or_else(|_| panic!("Primary number must fit into u32; qed"));
 
-        // Just ignore it if the receipt is too old and has been pruned.
-        if crate::aux_schema::target_receipt_is_pruned(head_receipt_number, primary_number) {
-            return Ok(());
-        }
-
-        let block_hash = execution_receipt.domain_hash;
-        let block_number = primary_number.into();
-
-        // TODO: more efficient execution receipt checking strategy?
-        let local_receipt = if let Some(local_receipt) =
-            crate::aux_schema::load_execution_receipt(&*self.client, block_hash)?
-        {
-            local_receipt
-        } else {
-            // Wait for the local execution receipt until it's ready.
-            let (tx, rx) = crossbeam::channel::bounded::<
-                sp_blockchain::Result<ExecutionReceiptFor<PBlock, Block::Hash>>,
-            >(1);
-            let executor = self.clone();
-            self.spawner.spawn(
-                "wait-for-local-execution-receipt",
-                None,
-                async move {
-                    if let Err(err) = executor
-                        .wait_for_local_future_receipt(block_hash, block_number, tx)
-                        .await
-                    {
-                        tracing::error!(
-                            target: LOG_TARGET,
-                            ?err,
-                            "Error occurred while waiting for the local receipt"
-                        );
-                    }
-                }
-                .boxed(),
-            );
-            rx.recv()??
-        };
-
-        // TODO: What happens for this obvious error?
-        if local_receipt.trace.len() != execution_receipt.trace.len() {}
-
-        if let Some(trace_mismatch_index) = find_trace_mismatch(&local_receipt, execution_receipt) {
-            let fraud_proof = self.fraud_proof_generator.generate_proof(
-                trace_mismatch_index,
-                &local_receipt,
+        if let Some(fraud_proof) = self
+            .gossip_message_validator
+            .validate_gossiped_execution_receipt(
                 signed_bundle_hash,
-            )?;
-
+                execution_receipt,
+                head_receipt_number,
+            )?
+        {
             self.primary_chain_client
                 .runtime_api()
                 .submit_fraud_proof_unsigned(
