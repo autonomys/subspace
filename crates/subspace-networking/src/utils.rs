a223bae32428e473f0d0c36a2b56d3ef96f6415f
Rahul Subramaniyam <rahulksnv@gmail.com>
2022-12-13 01:22:48 -0800

    Add ResizableSemaphore

--- crates/subspace-networking/src/utils.rs
+++ crates/subspace-networking/src/utils.rs
@@ -7,6 +7,8 @@ use libp2p::multiaddr::Protocol;
 use libp2p::{Multiaddr, PeerId};
 use std::marker::PhantomData;
 use std::num::NonZeroUsize;
+use std::sync::Arc;
+use tokio::sync::{Mutex, Notify};
 use tracing::warn;
 
 /// This test is successful only for global IP addresses and DNS names.
@@ -94,4 +96,124 @@ pub(crate) fn convert_multiaddresses(addresses: Vec<Multiaddr>) -> Vec<PeerAddre
             }
         })
         .collect()
+}
+
+/// Semaphore like implementation that allows both shrinking and expanding
+/// the max permits.
+#[derive(Clone)]
+pub(crate) struct ResizableSemaphore {
+    state: Arc<SemState>,
+}
+
+/// The semaphore permit.
+#[derive(Clone)]
+pub(crate) struct ResizableSemaphorePermit {
+    state: Arc<SemState>,
+}
+
+/// The state shared between the semaphore and the outstanding permits.
+struct SemState {
+    /// The tuple holds (current usage, current max capacity)
+    current: Mutex<(usize, usize)>,
+
+    /// To signal waiters for permits to be available.
+    notify: Notify,
+}
+
+impl SemState {
+    fn new(capacity: usize) -> Self {
+        Self {
+            current: Mutex::new((0, capacity)),
+            notify: Notify::new(),
+        }
+    }
+
+    // Allocates a permit if available.
+    // Returns true if a permit was allocated, false otherwise
+    async fn alloc_one(&self) -> bool {
+        let mut current = self.current.lock().await; // (usage, capacity)
+        if current.0 < current.1 {
+            current.0 += 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    // Returns a permit to the free pool, notifies waiters if needed.
+    async fn free_one(&self) {
+        let should_notify = {
+            let mut current = self.current.lock().await; // (usage, capacity)
+            assert!(current.0 > 0);
+            current.0 -= 1;
+
+            // Notify only if usage fell below the current capacity.
+            // For example: if the previous capacity was 100, and current capacity
+            // is 50, this will wait for usage to fall below 50 before any waiters
+            // are notified.
+            current.0 < current.1
+        };
+        if should_notify {
+            self.notify.notify_waiters();
+        }
+    }
+
+    // Expands the max capacity by delta, and notifies any waiters of the newly available
+    // free permits.
+    async fn expand(&self, delta: usize) {
+        {
+            let mut current = self.current.lock().await; // (usage, capacity)
+            current.1 += delta;
+        }
+        self.notify.notify_waiters();
+    }
+
+    // Shrinks the max capacity by delta
+    async fn shrink(&self, delta: usize) {
+        let mut current = self.current.lock().await; // (usage, capacity)
+        assert!(current.1 > delta);
+        current.1 -= delta;
+    }
+}
+
+impl ResizableSemaphore {
+    pub(crate) fn new(capacity: usize) -> Self {
+        Self {
+            state: Arc::new(SemState::new(capacity)),
+        }
+    }
+
+    /// Acquires a permit.
+    pub(crate) async fn acquire(&self) -> ResizableSemaphorePermit {
+        loop {
+            if self.state.alloc_one().await {
+                break;
+            }
+            self.state.notify.notified().await;
+        }
+        ResizableSemaphorePermit {
+            state: self.state.clone(),
+        }
+    }
+
+    /// Expands the capacity by specified amount.
+    pub(crate) async fn expand(&self, delta: usize) {
+        self.state.expand(delta).await;
+    }
+
+    /// Shrinks the capacity by specified amount.
+    pub(crate) async fn shrink(&self, delta: usize) {
+        self.state.shrink(delta).await;
+    }
+}
+
+impl Drop for ResizableSemaphorePermit {
+    fn drop(&mut self) {
+        let state = self.state.clone();
+        tokio::spawn({
+            async move {
+                state.free_one().await;
+            }
+        });
+    }
 }
