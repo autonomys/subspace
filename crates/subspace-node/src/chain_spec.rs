39a4bae865350c5ad9a1146e7fb83e3dda02e22a
Vedhavyas Singareddi <vedhavyas.singareddi@gmail.com>
2023-01-18 17:24:30 +0100

    add gemini-3c raw spec (#1074)
    * add gemini-3c raw spec

--- crates/subspace-node/src/chain_spec.rs
+++ crates/subspace-node/src/chain_spec.rs
@@ -31,7 +31,7 @@ use subspace_runtime_primitives::{AccountId, Balance, BlockNumber, SSC};
 use system_domain_runtime::GenesisConfig as SystemDomainGenesisConfig;
 
 const SUBSPACE_TELEMETRY_URL: &str = "wss://telemetry.subspace.network/submit/";
-// const GEMINI_3B_CHAIN_SPEC: &[u8] = include_bytes!("../res/chain-spec-raw-gemini-3b.json");
+const GEMINI_3C_CHAIN_SPEC: &[u8] = include_bytes!("../res/chain-spec-raw-gemini-3c.json");
 const X_NET_2_CHAIN_SPEC: &[u8] = include_bytes!("../res/chain-spec-raw-x-net-2.json");
 
 /// List of accounts which should receive token grants, amounts are specified in SSC.
@@ -71,7 +71,7 @@ struct GenesisParams {
 }
 
 pub fn gemini_3c() -> Result<ConsensusChainSpec<GenesisConfig, SystemDomainGenesisConfig>, String> {
-    unimplemented!("please run gemini-3c compiled chain")
+    ConsensusChainSpec::from_json_bytes(GEMINI_3C_CHAIN_SPEC)
 }
 
 pub fn gemini_3c_compiled(
