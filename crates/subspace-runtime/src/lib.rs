bfc83db2388a823f057d3dd4e32307b0eba5d148
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2023-01-27 07:30:20 +0800

    Bump runtime version
    Update the runtime version of consensus chain and system domain from 0 to
    1.

--- crates/subspace-runtime/src/lib.rs
+++ crates/subspace-runtime/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subspace"),
     impl_name: create_runtime_str!("subspace"),
     authoring_version: 0,
-    spec_version: 0,
+    spec_version: 1,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 0,
