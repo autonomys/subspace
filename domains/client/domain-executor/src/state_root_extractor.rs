b8ee0e5af497952e32ff58bba847906a31e3a9ee
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-03-18 21:32:47 +0100

    provides an implementation of the StateRootExtractor with just system domain runtime WASM.
    This implementation can be used on primary chain while doing fraud proof verification. The runtime code is expected as argument since we would have extracted the runtime from primary chain for fraud proof anyway. As for the executor, we can reuse the exisiting one on the primary chain.
    
    Note: this implementation will be used in next PRs.

--- domains/client/domain-executor/src/state_root_extractor.rs
+++ domains/client/domain-executor/src/state_root_extractor.rs
@@ -1,8 +1,14 @@
-use sp_api::{ApiError, BlockT, ProvideRuntimeApi};
+use codec::{Decode, Encode};
+use sc_executor::RuntimeVersionOf;
+use sp_api::{ApiError, BlockT, Core, ProvideRuntimeApi, RuntimeVersion};
 use sp_blockchain::HeaderBackend;
+use sp_core::traits::{CallContext, CodeExecutor, FetchRuntimeCode, RuntimeCode};
+use sp_core::{ExecutionContext, Hasher};
 use sp_messenger::messages::ExtractedStateRootsFromProof;
 use sp_messenger::MessengerApi;
 use sp_runtime::traits::NumberFor;
+use sp_state_machine::BasicExternalities;
+use std::borrow::Cow;
 use std::sync::Arc;
 
 type ExtractedStateRoots<Block> = ExtractedStateRootsFromProof<
@@ -56,5 +62,127 @@ where
             .and_then(|maybe_state_roots| {
                 maybe_state_roots.ok_or(ApiError::Application("Empty state roots".into()))
             })
+    }
+}
+
+/// This is a compatibility implementation to invoke stateless functions on MessengerApi.
+/// This will stateless wrapper and, hence, the dispatch may give invalid output if the invoked function
+/// relies on state of the chain.
+/// Note: Be sure to use this only when there is no System domain client available as this holds
+/// entire system domain runtime on heap.
+pub struct StateRootExtractorWithSystemDomainRuntime<Executor> {
+    executor: Arc<Executor>,
+    runtime_code: Cow<'static, [u8]>,
+}
+
+impl<SBlock, Executor> Core<SBlock> for StateRootExtractorWithSystemDomainRuntime<Executor>
+where
+    SBlock: BlockT,
+    Executor: CodeExecutor + RuntimeVersionOf,
+{
+    fn __runtime_api_internal_call_api_at(
+        &self,
+        _at: <SBlock as BlockT>::Hash,
+        _context: ExecutionContext,
+        params: Vec<u8>,
+        fn_name: &dyn Fn(RuntimeVersion) -> &'static str,
+    ) -> Result<Vec<u8>, ApiError> {
+        self.dispatch_call(fn_name, params)
+    }
+}
+
+impl<SBlock, Executor> MessengerApi<SBlock, NumberFor<SBlock>>
+    for StateRootExtractorWithSystemDomainRuntime<Executor>
+where
+    SBlock: BlockT,
+    NumberFor<SBlock>: Encode + Decode,
+    Executor: CodeExecutor + RuntimeVersionOf,
+{
+    fn __runtime_api_internal_call_api_at(
+        &self,
+        _at: <SBlock as BlockT>::Hash,
+        _context: ExecutionContext,
+        params: Vec<u8>,
+        fn_name: &dyn Fn(RuntimeVersion) -> &'static str,
+    ) -> Result<Vec<u8>, ApiError> {
+        self.dispatch_call(fn_name, params)
+    }
+}
+
+impl<Executor> FetchRuntimeCode for StateRootExtractorWithSystemDomainRuntime<Executor> {
+    fn fetch_runtime_code(&self) -> Option<Cow<[u8]>> {
+        Some(self.runtime_code.clone())
+    }
+}
+
+impl<Executor> StateRootExtractorWithSystemDomainRuntime<Executor>
+where
+    Executor: CodeExecutor + RuntimeVersionOf,
+{
+    pub fn new(executor: Arc<Executor>, runtime_code: Cow<'static, [u8]>) -> Self {
+        Self {
+            executor,
+            runtime_code,
+        }
+    }
+
+    fn runtime_code(&self) -> RuntimeCode {
+        let code_hash = sp_core::Blake2Hasher::hash(&self.runtime_code);
+        RuntimeCode {
+            code_fetcher: self,
+            heap_pages: None,
+            hash: code_hash.encode(),
+        }
+    }
+
+    fn runtime_version(&self) -> Result<RuntimeVersion, ApiError> {
+        let mut ext = BasicExternalities::new_empty();
+        self.executor
+            .runtime_version(&mut ext, &self.runtime_code())
+            .map_err(|err| ApiError::Application(Box::new(err)))
+    }
+
+    fn dispatch_call(
+        &self,
+        fn_name: &dyn Fn(RuntimeVersion) -> &'static str,
+        input: Vec<u8>,
+    ) -> Result<Vec<u8>, ApiError> {
+        let runtime_version = self.runtime_version()?;
+        let fn_name = fn_name(runtime_version);
+        let mut ext = BasicExternalities::new_empty();
+        self.executor
+            .call(
+                &mut ext,
+                &self.runtime_code(),
+                fn_name,
+                &input,
+                false,
+                CallContext::Offchain,
+            )
+            .0
+            .map_err(|err| {
+                ApiError::Application(format!("Failed to invoke call to {fn_name}: {err}").into())
+            })
+    }
+}
+
+impl<SBlock, Executor> StateRootExtractor<SBlock>
+    for StateRootExtractorWithSystemDomainRuntime<Executor>
+where
+    SBlock: BlockT,
+    Executor: CodeExecutor + RuntimeVersionOf,
+{
+    fn extract_state_roots(
+        &self,
+        ext: &SBlock::Extrinsic,
+    ) -> Result<ExtractedStateRoots<SBlock>, ApiError> {
+        <Self as MessengerApi<SBlock, NumberFor<SBlock>>>::extract_xdm_proof_state_roots(
+            self,
+            Default::default(),
+            ext,
+        )
+        .and_then(|maybe_state_roots| {
+            maybe_state_roots.ok_or(ApiError::Application("Empty state roots".into()))
+        })
     }
 }
