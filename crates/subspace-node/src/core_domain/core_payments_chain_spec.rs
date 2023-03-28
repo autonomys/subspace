61d8bcdce2fc7de058cd540f21755e67e64cf454
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-03-28 18:11:09 +0200

    Add gemini-3d chainspec and remove gemini-3c.

--- crates/subspace-node/src/core_domain/core_payments_chain_spec.rs
+++ crates/subspace-node/src/core_domain/core_payments_chain_spec.rs
@@ -110,16 +110,16 @@ pub fn local_testnet_config() -> ExecutionChainSpec<GenesisConfig> {
     )
 }
 
-pub fn gemini_3c_config() -> ExecutionChainSpec<GenesisConfig> {
+pub fn gemini_3d_config() -> ExecutionChainSpec<GenesisConfig> {
     ExecutionChainSpec::from_genesis(
         // Name
-        "Subspace Gemini 3c Core Payments Domain",
+        "Subspace Gemini 3d Core Payments Domain",
         // ID
-        "subspace_gemini_3c_core_payments_domain",
-        ChainType::Local,
+        "subspace_gemini_3d_core_payments_domain",
+        ChainType::Live,
         move || {
             let sudo_account =
-                AccountId::from_ss58check("5CXTmJEusve5ixyJufqHThmy4qUrrm6FyLCR7QfE4bbyMTNC")
+                AccountId::from_ss58check("5CZy4hcmaVZUMZLfB41v1eAKvtZ8W7axeWuDvwjhjPwfhAqt")
                     .expect("Invalid Sudo account");
             testnet_genesis(
                 vec![
@@ -138,7 +138,7 @@ pub fn gemini_3c_config() -> ExecutionChainSpec<GenesisConfig> {
         // Telemetry
         None,
         // Protocol ID
-        Some("subspace-gemini-3c-core-payments-domain"),
+        Some("subspace-gemini-3d-core-payments-domain"),
         None,
         // Properties
         Some(chain_spec_properties()),
@@ -191,7 +191,7 @@ pub fn devnet_config() -> ExecutionChainSpec<GenesisConfig> {
 pub fn load_chain_spec(spec_id: &str) -> std::result::Result<Box<dyn sc_cli::ChainSpec>, String> {
     let chain_spec = match spec_id {
         "dev" => development_config(),
-        "gemini-3c" => gemini_3c_config(),
+        "gemini-3d" => gemini_3d_config(),
         "devnet" => devnet_config(),
         "" | "local" => local_testnet_config(),
         path => ChainSpec::from_json_file(std::path::PathBuf::from(path))?,
