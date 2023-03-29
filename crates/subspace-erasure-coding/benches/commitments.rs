82e97f4f775dd8be9705760fb3681d4d2d8f833c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2023-03-29 14:47:55 +0300

    Update erasure coding benchmark

--- crates/subspace-erasure-coding/benches/commitments.rs
+++ crates/subspace-erasure-coding/benches/commitments.rs
@@ -3,11 +3,13 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use kzg::G1;
 use std::num::NonZeroUsize;
 use subspace_core_primitives::crypto::kzg::Commitment;
+use subspace_core_primitives::ArchivedHistorySegment;
 use subspace_erasure_coding::ErasureCoding;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let scale = NonZeroUsize::new(8).unwrap();
-    let num_shards = 2usize.pow(scale.get() as u32);
+    let num_shards = ArchivedHistorySegment::NUM_PIECES;
+    let scale = NonZeroUsize::new(num_shards.ilog2() as usize)
+        .expect("Recorded history segment contains at very least one record; qed");
     let ec = ErasureCoding::new(scale).unwrap();
 
     let source_commitments = (0..num_shards / 2)
