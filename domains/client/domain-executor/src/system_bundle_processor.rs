076a6189097e472eb8b6e993fe9d148b60c4ae64
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2023-03-31 21:29:28 +0800

    Kill preprocessor module in block-preprocessor and add some docs
    `domain_block_preprocessor::foo` looks a bit nicer than `domain_block_preprocessor::preprocessor::foo`.

--- domains/client/domain-executor/src/system_bundle_processor.rs
+++ domains/client/domain-executor/src/system_bundle_processor.rs
@@ -1,8 +1,8 @@
 use crate::domain_block_processor::{DomainBlockProcessor, PendingPrimaryBlocks};
 use crate::utils::translate_number_type;
 use crate::TransactionFor;
-use domain_block_preprocessor::preprocessor::SystemDomainBlockPreprocessor;
 use domain_block_preprocessor::runtime_api_full::RuntimeApiFull;
+use domain_block_preprocessor::SystemDomainBlockPreprocessor;
 use domain_runtime_primitives::{AccountId, DomainCoreApi};
 use sc_client_api::{AuxStore, BlockBackend, StateBackendFor};
 use sc_consensus::BlockImport;
