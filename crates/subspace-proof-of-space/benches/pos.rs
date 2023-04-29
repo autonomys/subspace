c50f7cc385e68c7dbd98735bc2f1660b1dce7e9e
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2023-04-30 02:29:31 +0300

    Make pos bench generic to reduce code duplication

--- crates/subspace-proof-of-space/benches/pos.rs
+++ crates/subspace-proof-of-space/benches/pos.rs
@@ -1,18 +1,78 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+#![feature(const_trait_impl)]
+
+#[cfg(any(feature = "chia", feature = "shim"))]
+use criterion::black_box;
+use criterion::{criterion_group, criterion_main, Criterion};
+#[cfg(any(feature = "chia", feature = "shim"))]
 use subspace_core_primitives::PosSeed;
-#[cfg(feature = "chia")]
-use subspace_proof_of_space::chia::ChiaTable;
-#[cfg(feature = "shim")]
-use subspace_proof_of_space::shim::ShimTable;
+#[cfg(any(feature = "chia", feature = "shim"))]
 use subspace_proof_of_space::{Quality, Table};
 
+#[cfg(any(feature = "chia", feature = "shim"))]
+const SEED: PosSeed = PosSeed::from([
+    35, 2, 52, 4, 51, 55, 23, 84, 91, 10, 111, 12, 13, 222, 151, 16, 228, 211, 254, 45, 92, 198,
+    204, 10, 9, 10, 11, 129, 139, 171, 15, 23,
+]);
+
+#[cfg(any(feature = "chia", feature = "shim"))]
+fn pos_bench<PosTable>(
+    c: &mut Criterion,
+    name: &'static str,
+    challenge_index_without_solution: u32,
+    challenge_index_with_solution: u32,
+) where
+    PosTable: Table,
+{
+    let mut group = c.benchmark_group(name);
+
+    group.bench_function("table", |b| {
+        b.iter(|| {
+            PosTable::generate(black_box(&SEED));
+        });
+    });
+
+    let table = PosTable::generate(&SEED);
+
+    group.bench_function("quality/no-solution", |b| {
+        b.iter(|| {
+            assert!(table
+                .find_quality(black_box(challenge_index_without_solution))
+                .is_none());
+        });
+    });
+
+    group.bench_function("quality/solution", |b| {
+        b.iter(|| {
+            assert!(table
+                .find_quality(black_box(challenge_index_with_solution))
+                .is_some());
+        });
+    });
+
+    let quality = table.find_quality(challenge_index_with_solution).unwrap();
+
+    group.bench_function("proof", |b| {
+        b.iter(|| {
+            quality.create_proof();
+        });
+    });
+
+    let proof = quality.create_proof();
+
+    group.bench_function("verification", |b| {
+        b.iter(|| {
+            assert!(
+                PosTable::is_proof_valid(&SEED, challenge_index_with_solution, &proof).is_some()
+            );
+        });
+    });
+    group.finish();
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let seed = PosSeed::from([
-        35, 2, 52, 4, 51, 55, 23, 84, 91, 10, 111, 12, 13, 222, 151, 16, 228, 211, 254, 45, 92,
-        198, 204, 10, 9, 10, 11, 129, 139, 171, 15, 23,
-    ]);
     #[cfg(not(any(feature = "chia", feature = "shim")))]
     {
+        let _ = c;
         panic!(r#"Enable "chia" and/or "shim" feature to run benches"#);
     }
     #[cfg(feature = "chia")]
@@ -22,51 +82,12 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         // This challenge index with above seed is known to have a solution
         let challenge_index_with_solution = 1;
 
-        let mut group = c.benchmark_group("chia");
-
-        group.bench_function("table", |b| {
-            b.iter(|| {
-                ChiaTable::generate(black_box(&seed));
-            });
-        });
-
-        let table = ChiaTable::generate(&seed);
-
-        group.bench_function("quality/no-solution", |b| {
-            b.iter(|| {
-                assert!(table
-                    .find_quality(black_box(challenge_index_without_solution))
-                    .is_none());
-            });
-        });
-
-        group.bench_function("quality/solution", |b| {
-            b.iter(|| {
-                assert!(table
-                    .find_quality(black_box(challenge_index_with_solution))
-                    .is_some());
-            });
-        });
-
-        let quality = table.find_quality(challenge_index_with_solution).unwrap();
-
-        group.bench_function("proof", |b| {
-            b.iter(|| {
-                quality.create_proof();
-            });
-        });
-
-        let proof = quality.create_proof();
-
-        group.bench_function("verification", |b| {
-            b.iter(|| {
-                assert!(
-                    ChiaTable::is_proof_valid(&seed, challenge_index_with_solution, &proof)
-                        .is_some()
-                );
-            });
-        });
-        group.finish();
+        pos_bench::<subspace_proof_of_space::chia::ChiaTable>(
+            c,
+            "chia",
+            challenge_index_without_solution,
+            challenge_index_with_solution,
+        )
     }
     #[cfg(feature = "shim")]
     {
@@ -75,51 +96,12 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         // This challenge index with above seed is known to have a solution
         let challenge_index_with_solution = 2;
 
-        let mut group = c.benchmark_group("shim");
-
-        group.bench_function("table", |b| {
-            b.iter(|| {
-                ShimTable::generate(black_box(&seed));
-            });
-        });
-
-        let table = ShimTable::generate(&seed);
-
-        group.bench_function("quality/no-solution", |b| {
-            b.iter(|| {
-                assert!(table
-                    .find_quality(black_box(challenge_index_without_solution))
-                    .is_none());
-            });
-        });
-
-        group.bench_function("quality/solution", |b| {
-            b.iter(|| {
-                assert!(table
-                    .find_quality(black_box(challenge_index_with_solution))
-                    .is_some());
-            });
-        });
-
-        let quality = table.find_quality(challenge_index_with_solution).unwrap();
-
-        group.bench_function("proof", |b| {
-            b.iter(|| {
-                quality.create_proof();
-            });
-        });
-
-        let proof = quality.create_proof();
-
-        group.bench_function("verification", |b| {
-            b.iter(|| {
-                assert!(
-                    ShimTable::is_proof_valid(&seed, challenge_index_with_solution, &proof)
-                        .is_some()
-                );
-            });
-        });
-        group.finish();
+        pos_bench::<subspace_proof_of_space::shim::ShimTable>(
+            c,
+            "shim",
+            challenge_index_without_solution,
+            challenge_index_with_solution,
+        )
     }
 }
 
