a938de0baf53be051804894ef66ef5ac92331b67
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-05-12 13:40:06 +0200

    backport: bump spec version for system and payments
    Runtime upgrade for either is optional but preferred if we want XDM enabled.

--- domains/runtime/system/src/runtime.rs
+++ domains/runtime/system/src/runtime.rs
@@ -92,7 +92,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subspace-system-domain"),
     impl_name: create_runtime_str!("subspace-system-domain"),
     authoring_version: 0,
-    spec_version: 2,
+    spec_version: 3,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 0,
