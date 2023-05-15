a2d64d823f4d2ebfcb4e56518da613ebb7bfec37
Rahul Subramaniyam <78006270+rahulksnv@users.noreply.github.com>
2023-05-15 16:31:37 -0700

    Compact block implementation (#1443)
    * Compact block changes
    
    * Refactoring, clean ups
    
    * Change DownloadResult, other cleanups
    
    * Replace encode_fn with req/rsp wrapper
    
    * Remove unnecessary `decode_response`, make requests easier to use
    
    * Remove unnecessary `ProtocolRequestResponse` and `ProtocolRequestResponseImpl`, make requests easier to use
    
    * Remove unnecessary `Block` generic from `RequestResponseWrapper`
    
    * Remove unnecessary `Block` generic from `NetworkWrapper`
    
    * Refactor `RequestResponseWrapper` into `NetworkPeerHandle`, remove a bunch of confusing `From<X> for Result<T, E>` conversions, add some TODOs
    
    * Follow clippy suggestion about `Default`
    
    * Rename and reformat for better readability
    
    * Use specific type for block attributes rather than converting to/from `u32` manually
    
    * Fix duplicated Substrate dependencies
    
    * Rename `subspace-block-relay` to `sc-subspace-block-relay`
    
    ---------
    
    Co-authored-by: Nazar Mokrynskyi <nazar@mokrynskyi.com>

---
+++ crates/sc-subspace-block-relay/src/utils.rs
@@ -0,0 +1,147 @@
+//! Common utils.
+
+use codec::{self, Decode, Encode};
+use futures::channel::oneshot;
+use parking_lot::Mutex;
+use sc_network::request_responses::IfDisconnected;
+use sc_network::types::ProtocolName;
+use sc_network::{NetworkRequest, PeerId, RequestFailure};
+use std::sync::Arc;
+
+type NetworkRequestService = Arc<dyn NetworkRequest + Send + Sync + 'static>;
+
+/// Wrapper to work around the circular dependency in substrate:
+/// `build_network()` requires the block relay to be passed in,
+/// which internally needs the network handle. `set()` is
+/// used to fill in the network after the network is created.
+pub struct NetworkWrapper {
+    network: Mutex<Option<NetworkRequestService>>,
+}
+
+impl Default for NetworkWrapper {
+    fn default() -> Self {
+        Self {
+            network: Mutex::new(None),
+        }
+    }
+}
+
+impl NetworkWrapper {
+    pub fn set(&self, network: NetworkRequestService) {
+        *self.network.lock() = Some(network);
+    }
+
+    pub(crate) fn network_peer_handle(
+        &self,
+        protocol_name: ProtocolName,
+        who: PeerId,
+    ) -> Result<NetworkPeerHandle, RequestResponseErr> {
+        match self.network.lock().as_ref().cloned() {
+            Some(network) => Ok(NetworkPeerHandle::new(protocol_name, who, network)),
+            None => Err(RequestResponseErr::NetworkUninitialized),
+        }
+    }
+}
+
+/// Network handle that allows making requests to specific peer and protocol.
+#[derive(Clone)]
+pub(crate) struct NetworkPeerHandle {
+    protocol_name: ProtocolName,
+    who: PeerId,
+    network: NetworkRequestService,
+}
+
+impl NetworkPeerHandle {
+    fn new(protocol_name: ProtocolName, who: PeerId, network: NetworkRequestService) -> Self {
+        Self {
+            protocol_name,
+            who,
+            network,
+        }
+    }
+
+    /// Performs the request
+    pub(crate) async fn request<Request, Response>(
+        &self,
+        request: Request,
+    ) -> Result<Response, RequestResponseErr>
+    where
+        Request: Encode,
+        Response: Decode,
+    {
+        let (tx, rx) = oneshot::channel();
+        self.network.start_request(
+            self.who,
+            self.protocol_name.clone(),
+            request.encode(),
+            tx,
+            IfDisconnected::ImmediateError,
+        );
+
+        let response_bytes = rx
+            .await
+            .map_err(|_cancelled| RequestResponseErr::Canceled)?
+            .map_err(RequestResponseErr::RequestFailure)?;
+
+        let response_len = response_bytes.len();
+        Response::decode(&mut response_bytes.as_ref())
+            .map_err(|err| RequestResponseErr::DecodeFailed { response_len, err })
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum RequestResponseErr {
+    #[error("RequestResponseErr::DecodeFailed: {response_len}/{err:?}")]
+    DecodeFailed {
+        response_len: usize,
+        err: codec::Error,
+    },
+
+    #[error("RequestResponseErr::RequestFailure {0:?}")]
+    RequestFailure(RequestFailure),
+
+    #[error("Network not initialized")]
+    NetworkUninitialized,
+
+    #[error("RequestResponseErr::Canceled")]
+    Canceled,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum RelayError {
+    #[error("Block header: {0}")]
+    BlockHeader(String),
+
+    #[error("Block indexed body: {0}")]
+    BlockIndexedBody(String),
+
+    #[error("Block justifications: {0}")]
+    BlockJustifications(String),
+
+    #[error("Block hash: {0}")]
+    BlockHash(String),
+
+    #[error("Block body: {0}")]
+    BlockBody(String),
+
+    #[error("Unexpected number of resolved entries: {expected}, {actual}")]
+    ResolveMismatch { expected: usize, actual: usize },
+
+    #[error("Resolved entry not found: {0}")]
+    ResolvedNotFound(usize),
+
+    #[error("Unexpected initial request")]
+    UnexpectedInitialRequest,
+
+    #[error("Unexpected initial response")]
+    UnexpectedInitialResponse,
+
+    #[error("Unexpected protocol request")]
+    UnexpectedProtocolRequest,
+
+    #[error("Unexpected protocol response")]
+    UnexpectedProtocolRespone,
+
+    #[error("Request/response error: {0}")]
+    RequestResponse(#[from] RequestResponseErr),
+}
