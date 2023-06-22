0c164d385f7dce64ade1c201db393fd6da91e5ee
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2023-06-23 06:58:27 +0800

    Remove outdated tests in pallet-domains

--- crates/pallet-domains/src/tests.rs
+++ crates/pallet-domains/src/tests.rs
@@ -1,18 +1,16 @@
 use crate::{self as pallet_domains};
 use frame_support::traits::{ConstU16, ConstU32, ConstU64, Hooks};
-use frame_support::{assert_noop, assert_ok, parameter_types};
+use frame_support::{assert_ok, parameter_types};
 use pallet_settlement::{PrimaryBlockHash, ReceiptVotes};
 use sp_core::crypto::Pair;
 use sp_core::{Get, H256, U256};
 use sp_domains::fraud_proof::{ExecutionPhase, FraudProof, InvalidStateTransitionProof};
-use sp_domains::transaction::InvalidTransactionCode;
 use sp_domains::{
     create_dummy_bundle_with_receipts_generic, BundleHeader, BundleSolution, DomainId,
     ExecutionReceipt, ExecutorPair, OpaqueBundle, SealedBundleHeader,
 };
 use sp_runtime::testing::Header;
-use sp_runtime::traits::{BlakeTwo256, IdentityLookup, ValidateUnsigned};
-use sp_runtime::transaction_validity::TransactionValidityError;
+use sp_runtime::traits::{BlakeTwo256, IdentityLookup};
 use sp_trie::StorageProof;
 use std::sync::atomic::{AtomicU64, Ordering};
 
@@ -151,6 +149,7 @@ fn create_dummy_bundle(
     }
 }
 
+#[allow(dead_code)]
 fn create_dummy_bundle_with_receipts(
     domain_id: DomainId,
     primary_number: BlockNumber,
@@ -163,165 +162,6 @@ fn create_dummy_bundle_with_receipts(
         primary_hash,
         receipt,
     )
-}
-
-#[test]
-fn submit_execution_receipt_incrementally_should_work() {
-    let (dummy_bundles, block_hashes): (Vec<_>, Vec<_>) = (1u64..=256u64 + 3u64)
-        .map(|n| {
-            let primary_hash = Hash::random();
-            (
-                create_dummy_bundle(DomainId::SYSTEM, n, primary_hash),
-                primary_hash,
-            )
-        })
-        .unzip();
-
-    let receipt_hash = |block_number| {
-        dummy_bundles[block_number as usize - 1]
-            .clone()
-            .receipt
-            .hash()
-    };
-
-    new_test_ext().execute_with(|| {
-        let genesis_hash = frame_system::Pallet::<Test>::block_hash(0);
-        PrimaryBlockHash::<Test>::insert(DomainId::SYSTEM, 0, genesis_hash);
-        Settlement::initialize_genesis_receipt(DomainId::SYSTEM, genesis_hash);
-
-        (0..256).for_each(|index| {
-            let block_hash = block_hashes[index];
-            PrimaryBlockHash::<Test>::insert(DomainId::SYSTEM, (index + 1) as u64, block_hash);
-
-            assert_ok!(pallet_domains::Pallet::<Test>::pre_dispatch(
-                &pallet_domains::Call::submit_bundle {
-                    opaque_bundle: dummy_bundles[index].clone()
-                }
-            ));
-            assert_ok!(Domains::submit_bundle(
-                RuntimeOrigin::none(),
-                dummy_bundles[index].clone(),
-            ));
-
-            assert_eq!(Settlement::finalized_receipt_number(DomainId::SYSTEM), 0);
-        });
-
-        assert!(Settlement::receipts(DomainId::SYSTEM, receipt_hash(257)).is_none());
-        assert_ok!(Domains::submit_bundle(
-            RuntimeOrigin::none(),
-            dummy_bundles[256].clone(),
-        ));
-        // The oldest ER should be deleted.
-        assert!(Settlement::receipts(DomainId::SYSTEM, receipt_hash(1)).is_none());
-        assert_eq!(Settlement::finalized_receipt_number(DomainId::SYSTEM), 1);
-        assert!(Settlement::receipts(DomainId::SYSTEM, receipt_hash(257)).is_some());
-
-        assert!(Settlement::receipts(DomainId::SYSTEM, receipt_hash(2)).is_some());
-        assert!(Settlement::receipts(DomainId::SYSTEM, receipt_hash(258)).is_none());
-
-        assert_noop!(
-            pallet_domains::Pallet::<Test>::pre_dispatch(&pallet_domains::Call::submit_bundle {
-                opaque_bundle: dummy_bundles[258].clone()
-            }),
-            TransactionValidityError::Invalid(InvalidTransactionCode::ExecutionReceipt.into())
-        );
-
-        assert_ok!(Domains::submit_bundle(
-            RuntimeOrigin::none(),
-            dummy_bundles[257].clone(),
-        ));
-        assert!(Settlement::receipts(DomainId::SYSTEM, receipt_hash(2)).is_none());
-        assert_eq!(Settlement::finalized_receipt_number(DomainId::SYSTEM), 2);
-        assert!(Settlement::receipts(DomainId::SYSTEM, receipt_hash(258)).is_some());
-    });
-}
-
-#[test]
-fn submit_execution_receipt_with_huge_gap_should_work() {
-    let (dummy_bundles, block_hashes): (Vec<_>, Vec<_>) = (1u64..=256u64 + 2)
-        .map(|n| {
-            let primary_hash = Hash::random();
-            (
-                create_dummy_bundle(DomainId::SYSTEM, n, primary_hash),
-                primary_hash,
-            )
-        })
-        .unzip();
-
-    let run_to_block = |n: BlockNumber, block_hashes: Vec<Hash>| {
-        System::initialize(&1, &System::parent_hash(), &Default::default());
-        <Domains as Hooks<BlockNumber>>::on_initialize(1);
-        System::finalize();
-
-        for b in 2..=n {
-            System::set_block_number(b);
-            System::initialize(&b, &block_hashes[b as usize - 2], &Default::default());
-            <Domains as Hooks<BlockNumber>>::on_initialize(b);
-            System::finalize();
-        }
-    };
-
-    new_test_ext().execute_with(|| {
-        run_to_block(256 + 2, block_hashes);
-
-        // Submit ancient receipts still works even the block hash mapping for [1, 256)
-        // in System has been removed.
-        assert!(!frame_system::BlockHash::<Test>::contains_key(1));
-        assert!(!frame_system::BlockHash::<Test>::contains_key(255));
-        (0..255).for_each(|index| {
-            assert_ok!(Domains::submit_bundle(
-                RuntimeOrigin::none(),
-                dummy_bundles[index].clone(),
-            ));
-        });
-
-        // Reaching the receipts pruning depth, block hash mapping will be pruned as well.
-        assert!(PrimaryBlockHash::<Test>::contains_key(DomainId::SYSTEM, 0));
-        assert_ok!(Domains::submit_bundle(
-            RuntimeOrigin::none(),
-            dummy_bundles[255].clone(),
-        ));
-        assert!(!PrimaryBlockHash::<Test>::contains_key(DomainId::SYSTEM, 0));
-
-        assert!(PrimaryBlockHash::<Test>::contains_key(DomainId::SYSTEM, 1));
-        assert_ok!(Domains::submit_bundle(
-            RuntimeOrigin::none(),
-            dummy_bundles[256].clone(),
-        ));
-        assert!(!PrimaryBlockHash::<Test>::contains_key(DomainId::SYSTEM, 1));
-
-        assert!(PrimaryBlockHash::<Test>::contains_key(DomainId::SYSTEM, 2));
-        assert_ok!(Domains::submit_bundle(
-            RuntimeOrigin::none(),
-            dummy_bundles[257].clone(),
-        ));
-        assert!(!PrimaryBlockHash::<Test>::contains_key(DomainId::SYSTEM, 2));
-        assert_eq!(Settlement::finalized_receipt_number(DomainId::SYSTEM), 2);
-    });
-}
-
-#[test]
-fn only_system_domain_receipts_are_maintained_on_primary_chain() {
-    let primary_hash = Hash::random();
-
-    let system_receipt = create_dummy_receipt(1, primary_hash);
-    let system_bundle = create_dummy_bundle_with_receipts(
-        DomainId::SYSTEM,
-        1,
-        primary_hash,
-        system_receipt.clone(),
-    );
-    let core_receipt = create_dummy_receipt(1, primary_hash);
-    let core_bundle =
-        create_dummy_bundle_with_receipts(DomainId::new(1), 1, primary_hash, core_receipt.clone());
-
-    new_test_ext().execute_with(|| {
-        assert_ok!(Domains::submit_bundle(RuntimeOrigin::none(), system_bundle));
-        assert_ok!(Domains::submit_bundle(RuntimeOrigin::none(), core_bundle));
-        // Only system domain receipt is tracked, core domain receipt is ignored.
-        assert!(Settlement::receipts(DomainId::SYSTEM, system_receipt.hash()).is_some());
-        assert!(Settlement::receipts(DomainId::SYSTEM, core_receipt.hash()).is_none());
-    });
 }
 
 #[test]
