7a7799fbfa7d8a9427bf04b56c5cf949d6761ec3
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-06-26 15:35:50 +0530

    add runtime registry types, storages and registration runtime call.

---
+++ crates/pallet-domains/src/runtime_registry.rs
@@ -0,0 +1,149 @@
+//! Runtime registry for domains
+// TODO: remove once connected
+#![allow(dead_code)]
+
+use crate::pallet::{NextRuntimeId, RuntimeRegistry};
+use crate::Config;
+use codec::{Decode, Encode};
+use frame_support::PalletError;
+use scale_info::TypeInfo;
+use sp_core::Hasher;
+use sp_domains::{RuntimeId, RuntimeType};
+use sp_std::vec::Vec;
+use sp_version::RuntimeVersion;
+
+/// Runtime specific errors
+#[derive(TypeInfo, Encode, Decode, PalletError, Debug, PartialEq)]
+pub enum Error {
+    FailedToExtractRuntimeVersion,
+    InvalidSpecName,
+    SpecVersionNeedsToIncrease,
+    MaxRuntimeId,
+}
+
+#[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq)]
+pub struct RuntimeObject<Number, Hash>
+where
+    Number: Encode + Decode + Clone,
+    Hash: Encode + Decode + Clone,
+{
+    pub runtime_name: Vec<u8>,
+    pub runtime_type: RuntimeType,
+    pub runtime_upgrades: u32,
+    pub hash: Hash,
+    pub code: Vec<u8>,
+    pub version: RuntimeVersion,
+    pub created_at: Number,
+    pub updated_at: Number,
+}
+
+/// Extracts the runtime version of the provided code.
+pub(crate) fn runtime_version(code: &[u8]) -> Result<RuntimeVersion, Error> {
+    sp_io::misc::runtime_version(code)
+        .and_then(|v| RuntimeVersion::decode(&mut &v[..]).ok())
+        .ok_or(Error::FailedToExtractRuntimeVersion)
+}
+
+/// Upgrades current runtime with new runtime.
+pub(crate) fn can_upgrade_code(
+    current_version: RuntimeVersion,
+    update_code: &[u8],
+) -> Result<RuntimeVersion, Error> {
+    let new_version = runtime_version(update_code)?;
+
+    if new_version.spec_name != current_version.spec_name {
+        return Err(Error::InvalidSpecName);
+    }
+
+    if new_version.spec_version <= current_version.spec_version {
+        return Err(Error::SpecVersionNeedsToIncrease);
+    }
+
+    Ok(new_version)
+}
+
+/// Registers a new domain runtime..
+pub(crate) fn do_register_runtime<T: Config>(
+    runtime_name: Vec<u8>,
+    runtime_type: RuntimeType,
+    code: Vec<u8>,
+    at: T::BlockNumber,
+) -> Result<RuntimeId, Error> {
+    let runtime_version = runtime_version(&code)?;
+    let runtime_hash = T::Hashing::hash(&code);
+    let runtime_id = NextRuntimeId::<T>::get();
+
+    RuntimeRegistry::<T>::insert(
+        runtime_id,
+        RuntimeObject {
+            runtime_name,
+            runtime_type,
+            hash: runtime_hash,
+            code,
+            version: runtime_version,
+            created_at: at,
+            updated_at: at,
+            runtime_upgrades: 0u32,
+        },
+    );
+
+    let next_runtime_id = runtime_id.checked_add(1).ok_or(Error::MaxRuntimeId)?;
+    NextRuntimeId::<T>::set(next_runtime_id);
+
+    Ok(runtime_id)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pallet::RuntimeRegistry;
+    use crate::tests::{new_test_ext, Test};
+    use codec::Encode;
+    use frame_support::assert_ok;
+    use frame_support::dispatch::RawOrigin;
+    use sp_domains::RuntimeType;
+    use sp_version::RuntimeVersion;
+
+    struct ReadRuntimeVersion(Vec<u8>);
+
+    impl sp_core::traits::ReadRuntimeVersion for ReadRuntimeVersion {
+        fn read_runtime_version(
+            &self,
+            _wasm_code: &[u8],
+            _ext: &mut dyn sp_externalities::Externalities,
+        ) -> Result<Vec<u8>, String> {
+            Ok(self.0.clone())
+        }
+    }
+
+    #[test]
+    fn create_domain_runtime() {
+        let version = RuntimeVersion {
+            spec_name: "test".into(),
+            impl_name: Default::default(),
+            authoring_version: 0,
+            spec_version: 1,
+            impl_version: 1,
+            apis: Default::default(),
+            transaction_version: 1,
+            state_version: 0,
+        };
+        let read_runtime_version = ReadRuntimeVersion(version.encode());
+
+        let mut ext = new_test_ext();
+        ext.register_extension(sp_core::traits::ReadRuntimeVersionExt::new(
+            read_runtime_version,
+        ));
+        ext.execute_with(|| {
+            let res = crate::Pallet::<Test>::register_domain_runtime(
+                RawOrigin::Root.into(),
+                b"evm".to_vec(),
+                RuntimeType::Evm,
+                vec![1, 2, 3, 4],
+            );
+
+            assert_ok!(res);
+            let runtime_obj = RuntimeRegistry::<Test>::get(0).unwrap();
+            assert_eq!(runtime_obj.version, version)
+        })
+    }
+}
