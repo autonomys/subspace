5dbf26eb1b5d4395044f8df069033adb40ac0d84
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2023-07-07 12:29:13 +0300

    Remove legacy Chia proof of space implementation, we don't need it anymore

--- crates/subspace-proof-of-space/benches/pos.rs
+++ crates/subspace-proof-of-space/benches/pos.rs
@@ -1,16 +1,16 @@
 #![feature(const_trait_impl)]
 
-#[cfg(any(feature = "chia-legacy", feature = "chia", feature = "shim"))]
+#[cfg(any(feature = "chia", feature = "shim"))]
 use criterion::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
 #[cfg(feature = "parallel")]
 use rayon::ThreadPoolBuilder;
-#[cfg(any(feature = "chia-legacy", feature = "chia", feature = "shim"))]
+#[cfg(any(feature = "chia", feature = "shim"))]
 use subspace_core_primitives::PosSeed;
-#[cfg(any(feature = "chia-legacy", feature = "chia", feature = "shim"))]
+#[cfg(any(feature = "chia", feature = "shim"))]
 use subspace_proof_of_space::{Quality, Table};
 
-#[cfg(any(feature = "chia-legacy", feature = "chia", feature = "shim"))]
+#[cfg(any(feature = "chia", feature = "shim"))]
 fn pos_bench<PosTable>(
     c: &mut Criterion,
     name: &'static str,
@@ -89,24 +89,10 @@ fn pos_bench<PosTable>(
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    #[cfg(not(any(feature = "chia-legacy", feature = "chia", feature = "shim")))]
+    #[cfg(not(any(feature = "chia", feature = "shim")))]
     {
         let _ = c;
         panic!(r#"Enable "chia" and/or "shim" feature to run benches"#);
-    }
-    #[cfg(feature = "chia-legacy")]
-    {
-        // This challenge index with above seed is known to not have a solution
-        let challenge_index_without_solution = 0;
-        // This challenge index with above seed is known to have a solution
-        let challenge_index_with_solution = 1;
-
-        pos_bench::<subspace_proof_of_space::chia_legacy::ChiaTable>(
-            c,
-            "chia-legacy",
-            challenge_index_without_solution,
-            challenge_index_with_solution,
-        )
     }
     #[cfg(feature = "chia")]
     {
