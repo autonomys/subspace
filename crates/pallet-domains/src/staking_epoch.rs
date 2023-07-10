cdccaf88777a054a0fa36dd868be0b445d094166
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-07-10 14:02:17 +0200

    define error type for epoch transitions

--- crates/pallet-domains/src/staking_epoch.rs
+++ crates/pallet-domains/src/staking_epoch.rs
@@ -6,17 +6,18 @@ use crate::pallet::{
     DomainStakingSummary, Nominators, Operators, PendingOperatorDeregistrations,
     PendingOperatorSwitches, PendingOperatorUnlocks,
 };
-use crate::{Config, FreezeIdentifier, NominatorId};
-use frame_support::log::error;
+use crate::{Config, FreezeIdentifier};
+use codec::{Decode, Encode};
+use frame_support::dispatch::TypeInfo;
 use frame_support::traits::fungible::{InspectFreeze, Mutate, MutateFreeze};
+use frame_support::PalletError;
 use sp_core::Get;
 use sp_domains::{DomainId, OperatorId};
 use sp_runtime::traits::{CheckedAdd, CheckedSub, Zero};
 use sp_runtime::Perbill;
-use sp_std::vec::Vec;
 
-#[derive(Debug)]
-enum Error {
+#[derive(TypeInfo, Encode, Decode, PalletError, Debug, PartialEq)]
+pub enum TransitionError {
     MissingOperator,
     OperatorFrozen,
     MissingDomainStakeSummary,
@@ -25,32 +26,44 @@ enum Error {
     ShareUnderflow,
     RemoveLock,
     MintBalance,
+    BlockNumberOverflow,
+}
+
+#[derive(TypeInfo, Encode, Decode, PalletError, Debug, PartialEq)]
+pub enum Error {
+    SwitchOperatorDomain(TransitionError),
+    FinalizeOperatorDeregistration(TransitionError),
+    UnlockOperator(TransitionError),
 }
 
 /// Add all the switched operators to new domain.
-pub(crate) fn do_finalize_switch_operator_domain<T: Config>(domain_id: DomainId) {
+pub(crate) fn do_finalize_switch_operator_domain<T: Config>(
+    domain_id: DomainId,
+) -> Result<(), Error> {
     if let Some(operators) = PendingOperatorSwitches::<T>::take(domain_id) {
-        for operator_id in operators {
-            if let Err(err) = switch_operator::<T>(operator_id) {
-                error!("Failed to switch operator[{operator_id:?}]: {err:?}",)
-            }
-        }
+        operators.into_iter().try_for_each(|operator_id| {
+            switch_operator::<T>(operator_id).map_err(Error::SwitchOperatorDomain)
+        })?;
     }
+
+    Ok(())
 }
 
-fn switch_operator<T: Config>(operator_id: OperatorId) -> Result<(), Error> {
+fn switch_operator<T: Config>(operator_id: OperatorId) -> Result<(), TransitionError> {
     Operators::<T>::try_mutate(operator_id, |maybe_operator| {
-        let operator = maybe_operator.as_mut().ok_or(Error::MissingOperator)?;
+        let operator = maybe_operator
+            .as_mut()
+            .ok_or(TransitionError::MissingOperator)?;
 
         if operator.is_frozen {
-            return Err(Error::OperatorFrozen);
+            return Err(TransitionError::OperatorFrozen);
         }
 
         operator.current_domain_id = operator.next_domain_id;
         DomainStakingSummary::<T>::try_mutate(operator.current_domain_id, |maybe_stake_summary| {
             let stake_summary = maybe_stake_summary
                 .as_mut()
-                .ok_or(Error::MissingDomainStakeSummary)?;
+                .ok_or(TransitionError::MissingDomainStakeSummary)?;
 
             stake_summary.next_operators.push(operator_id);
 
@@ -62,79 +75,73 @@ fn switch_operator<T: Config>(operator_id: OperatorId) -> Result<(), Error> {
 pub(crate) fn do_finalize_operator_deregistrations<T: Config>(
     domain_id: DomainId,
     consensus_block_number: T::BlockNumber,
-) {
+) -> Result<(), Error> {
     let stake_withdrawal_locking_period = T::StakeWithdrawalLockingPeriod::get();
-    let unlock_block_number = match consensus_block_number
+    let unlock_block_number = consensus_block_number
         .checked_add(&stake_withdrawal_locking_period)
-    {
-        None => {
-            error!("Failed to compute unlock domain block number: {consensus_block_number:?} + {stake_withdrawal_locking_period:?}",);
-            return;
-        }
-        Some(unlock_block_number) => unlock_block_number,
-    };
+        .ok_or(Error::FinalizeOperatorDeregistration(
+            TransitionError::BlockNumberOverflow,
+        ))?;
 
     if let Some(operators) = PendingOperatorDeregistrations::<T>::take(domain_id) {
         PendingOperatorUnlocks::<T>::insert(domain_id, unlock_block_number, operators)
     }
+
+    Ok(())
 }
 
 pub(crate) fn do_unlock_operators<T: Config>(
     domain_id: DomainId,
     domain_block_number: T::BlockNumber,
-) {
+) -> Result<(), Error> {
     if let Some(operators) = PendingOperatorUnlocks::<T>::take(domain_id, domain_block_number) {
-        for operator_id in operators {
-            if let Err(err) = unlock_operator::<T>(operator_id) {
-                error!("Failed to unlock operator pool[{operator_id:?}]: {err:?}",)
-            }
-        }
+        operators.into_iter().try_for_each(|operator_id| {
+            unlock_operator::<T>(operator_id).map_err(Error::UnlockOperator)
+        })?;
     }
+
+    Ok(())
 }
 
-fn unlock_operator<T: Config>(operator_id: OperatorId) -> Result<(), Error> {
+fn unlock_operator<T: Config>(operator_id: OperatorId) -> Result<(), TransitionError> {
     Operators::<T>::try_mutate(operator_id, |maybe_operator| {
-        let operator = maybe_operator.as_mut().ok_or(Error::MissingOperator)?;
+        let operator = maybe_operator
+            .as_mut()
+            .ok_or(TransitionError::MissingOperator)?;
 
         let mut total_shares = operator.total_shares;
         let mut total_stake = operator
             .current_total_stake
             .checked_add(&operator.current_epoch_rewards)
-            .ok_or(Error::BalanceOverflow)?;
-
-        let nominator_ids =
-            Nominators::<T>::iter_key_prefix(operator_id).collect::<Vec<NominatorId<T>>>();
+            .ok_or(TransitionError::BalanceOverflow)?;
 
         let freeze_identifier = T::FreezeIdentifier::staking_freeze_id(operator_id);
-        for nominator_id in nominator_ids {
-            // defensively read the the Nominator stake and then clear it once processed
-            if let Some(nominator) = Nominators::<T>::get(operator_id, nominator_id.clone()) {
-                let nominator_share = Perbill::from_rational(nominator.shares, total_shares);
-                let nominator_staked_amount = nominator_share * total_stake;
 
-                let locked_amount = T::Currency::balance_frozen(&freeze_identifier, &nominator_id);
-                let amount_to_mint = nominator_staked_amount
-                    .checked_sub(&locked_amount)
-                    .unwrap_or(Zero::zero());
+        Nominators::<T>::drain_prefix(operator_id).try_for_each(|(nominator_id, nominator)| {
+            let nominator_share = Perbill::from_rational(nominator.shares, total_shares);
+            let nominator_staked_amount = nominator_share * total_stake;
 
-                // remove the lock and mint any gains
-                T::Currency::thaw(&freeze_identifier, &nominator_id)
-                    .map_err(|_| Error::RemoveLock)?;
-                T::Currency::mint_into(&nominator_id, amount_to_mint)
-                    .map_err(|_| Error::MintBalance)?;
+            let locked_amount = T::Currency::balance_frozen(&freeze_identifier, &nominator_id);
+            let amount_to_mint = nominator_staked_amount
+                .checked_sub(&locked_amount)
+                .unwrap_or(Zero::zero());
 
-                // update pool's remaining shares and stake
-                total_shares = total_shares
-                    .checked_sub(&nominator.shares)
-                    .ok_or(Error::ShareUnderflow)?;
-                total_stake = total_stake
-                    .checked_sub(&nominator_staked_amount)
-                    .ok_or(Error::BalanceUnderflow)?;
+            // remove the lock and mint any gains
+            T::Currency::thaw(&freeze_identifier, &nominator_id)
+                .map_err(|_| TransitionError::RemoveLock)?;
+            T::Currency::mint_into(&nominator_id, amount_to_mint)
+                .map_err(|_| TransitionError::MintBalance)?;
 
-                // remove nominator
-                Nominators::<T>::remove(operator_id, nominator_id)
-            }
-        }
+            // update pool's remaining shares and stake
+            total_shares = total_shares
+                .checked_sub(&nominator.shares)
+                .ok_or(TransitionError::ShareUnderflow)?;
+            total_stake = total_stake
+                .checked_sub(&nominator_staked_amount)
+                .ok_or(TransitionError::BalanceUnderflow)?;
+
+            Ok(())
+        })?;
 
         // TODO: transfer any remaining amount to treasury
 
@@ -235,7 +242,7 @@ mod tests {
             });
 
             PendingOperatorSwitches::<Test>::append(old_domain_id, operator_id);
-            do_finalize_switch_operator_domain::<Test>(old_domain_id);
+            assert!(do_finalize_switch_operator_domain::<Test>(old_domain_id).is_ok());
 
             let operator = Operators::<Test>::get(operator_id).unwrap();
             assert_eq!(operator.current_domain_id, new_domain_id);
@@ -295,9 +302,9 @@ mod tests {
                 },
             });
 
-            let domain_block_number = 100;
-            PendingOperatorUnlocks::<Test>::append(domain_id, domain_block_number, operator_id);
-            do_unlock_operators::<Test>(domain_id, domain_block_number);
+            let consensus_block_number = 100;
+            PendingOperatorUnlocks::<Test>::append(domain_id, consensus_block_number, operator_id);
+            assert!(do_unlock_operators::<Test>(domain_id, consensus_block_number).is_ok());
 
             for nominator in &nominators {
                 let mut required_minimum_free_balance = minimum_free_balance + nominator.1;
@@ -315,7 +322,7 @@ mod tests {
             assert_eq!(operator.current_epoch_rewards, Zero::zero());
             assert_eq!(operator.current_total_stake, Zero::zero());
             assert_eq!(
-                PendingOperatorUnlocks::<Test>::get(domain_id, domain_block_number),
+                PendingOperatorUnlocks::<Test>::get(domain_id, consensus_block_number),
                 None
             )
         });
@@ -361,7 +368,11 @@ mod tests {
 
             PendingOperatorDeregistrations::<Test>::append(domain_id, operator_id);
             let current_consensus_block_number = 100;
-            do_finalize_operator_deregistrations::<Test>(domain_id, current_consensus_block_number);
+            assert!(do_finalize_operator_deregistrations::<Test>(
+                domain_id,
+                current_consensus_block_number,
+            )
+            .is_ok());
 
             let expected_unlock = 100 + crate::tests::StakeWithdrawalLockingPeriod::get();
             assert_eq!(
