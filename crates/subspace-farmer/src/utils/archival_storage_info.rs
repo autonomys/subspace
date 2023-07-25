921e6dc71106b9d685d431a34f6040f77f6f44cd
Shamil Gadelshin <shamilgadelshin@gmail.com>
2023-07-25 14:02:34 +0700

    farmer: Simplify cuckoo filter updates.

--- crates/subspace-farmer/src/utils/archival_storage_info.rs
+++ crates/subspace-farmer/src/utils/archival_storage_info.rs
@@ -1,19 +1,13 @@
 use cuckoofilter::{CuckooFilter, ExportedCuckooFilter};
 use parking_lot::Mutex;
-use rand::rngs::StdRng;
-use rand::{Rng, SeedableRng};
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::fmt;
 use std::fmt::Debug;
-use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 use subspace_core_primitives::PieceIndex;
 use subspace_networking::libp2p::PeerId;
 use subspace_networking::CuckooFilterDTO;
-use tracing::debug;
-
-const CONNECTED_PEERS_NUMBER_LIMIT: usize = 50;
 
 #[derive(Clone, Default)]
 pub struct ArchivalStorageInfo {
@@ -37,31 +31,7 @@ impl ArchivalStorageInfo {
 
         let cuckoo_filter = CuckooFilter::from(exported_filter);
 
-        let mut peer_filters = self.peers.lock();
-
-        peer_filters.insert(peer_id, cuckoo_filter);
-
-        // Truncate current peer set by limits.
-        let mut rng = StdRng::seed_from_u64({
-            // Hash of PeerID
-            let mut s = DefaultHasher::new();
-            peer_id.hash(&mut s);
-            s.finish()
-        });
-
-        // Remove random peer when we exceed the limit of storing peers (and their cuckoo-filters).
-        if peer_filters.len() > CONNECTED_PEERS_NUMBER_LIMIT {
-            let connected_peers = peer_filters.keys().cloned().collect::<Vec<_>>();
-            let random_index = rng.gen_range(0..connected_peers.len());
-
-            let removing_peer_id = *connected_peers
-                .get(random_index)
-                .expect("Index is checked to be present.");
-
-            peer_filters.remove(&removing_peer_id);
-
-            debug!(%removing_peer_id, "Removed disconnected peer from filter cache.");
-        }
+        self.peers.lock().insert(peer_id, cuckoo_filter);
     }
 
     pub fn remove_peer_filter(&self, peer_id: &PeerId) -> bool {
