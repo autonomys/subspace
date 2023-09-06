0b7e47b4d062efb5df0d41f805572d22c8b6f5ea
linning <linningde25@gmail.com>
2023-09-07 02:24:26 +0800

    Implement fraud proof processing
    The processing includes 1. remove the fraudulent ER and all of its descendants
    from the block tree 2. protentially update the heade receipt number 3. slash the
    operator who have submitted the pruned fraudulent ER
    
    Signed-off-by: linning <linningde25@gmail.com>

--- crates/pallet-domains/src/lib.rs
+++ crates/pallet-domains/src/lib.rs
@@ -110,8 +110,9 @@ mod pallet {
     };
     use crate::staking::{
         do_auto_stake_block_rewards, do_deregister_operator, do_nominate_operator,
-        do_register_operator, do_reward_operators, do_switch_operator_domain, do_withdraw_stake,
-        Error as StakingError, Nominator, Operator, OperatorConfig, StakingSummary, Withdraw,
+        do_register_operator, do_reward_operators, do_slash_operators, do_switch_operator_domain,
+        do_withdraw_stake, Error as StakingError, Nominator, Operator, OperatorConfig,
+        StakingSummary, Withdraw,
     };
     use crate::staking_epoch::{
         do_finalize_domain_current_epoch, do_unlock_pending_withdrawals,
@@ -674,6 +675,10 @@ mod pallet {
             domain_id: DomainId,
             completed_epoch_index: EpochIndex,
         },
+        FraudProofProcessed {
+            domain_id: DomainId,
+            new_head_receipt_number: Option<T::DomainNumber>,
+        },
     }
 
     /// Per-domain state for tx range calculation.
@@ -821,7 +826,76 @@ mod pallet {
 
             log::trace!(target: "runtime::domains", "Processing fraud proof: {fraud_proof:?}");
 
-            // TODO: Implement fraud proof processing.
+            let domain_id = fraud_proof.domain_id();
+            let mut receipt_to_remove = vec![fraud_proof.bad_receipt_hash()];
+            let mut operator_to_slash = BTreeSet::new();
+            let mut next_head_receipt_number = None;
+
+            // Prune the bad ER and all of its descendants from the block tree. ER are pruning
+            // with BFS order from lower height to higher height.
+            while let Some(receipt_hash) = receipt_to_remove.pop() {
+                let DomainBlock {
+                    execution_receipt,
+                    operator_ids,
+                } = DomainBlocks::<T>::take(receipt_hash)
+                    .ok_or::<Error<T>>(FraudProofError::BadReceiptNotFound.into())?;
+
+                BlockTree::<T>::mutate_exists(
+                    domain_id,
+                    execution_receipt.domain_block_number,
+                    |maybe_er_hashes| {
+                        if let Some(er_hashes) = maybe_er_hashes {
+                            // Remove ER hash from the set, remove the whole set if it is empty.
+                            er_hashes.remove(&receipt_hash);
+                            if er_hashes.is_empty() {
+                                maybe_er_hashes.take();
+                            }
+                            // If all the ER at `domain_block_number` are pruned then any ER that derive from domain
+                            // block with height > `domain_block_number` must also be pruned since their parent ER
+                            // are pruned thus we can reset the new head receipt number to `domain_block_number - 1`.
+                            if maybe_er_hashes.is_none() && next_head_receipt_number.is_none() {
+                                next_head_receipt_number
+                                    .replace(execution_receipt.domain_block_number - One::one());
+                            } else if maybe_er_hashes.is_some()
+                                && next_head_receipt_number.is_some()
+                            {
+                                // `next_head_receipt_number` is `Some` means all the ER at prior height are pruned
+                                // thus the descendants must also be pruned
+                                panic!("The descendants of pruned ER must also be pruned; qed");
+                            }
+                        }
+                    },
+                );
+
+                _ = StateRoots::<T>::take((
+                    domain_id,
+                    execution_receipt.domain_block_number,
+                    execution_receipt.domain_block_hash,
+                ));
+
+                // Add all the immediate descendants of the pruned ER to the `receipt_to_remove` list
+                DomainBlockDescendants::<T>::take(receipt_hash)
+                    .into_iter()
+                    .for_each(|descendant| receipt_to_remove.push(descendant));
+
+                // NOTE: the operator id will be deduplicated since we are using `BTreeSet`
+                operator_ids.into_iter().for_each(|id| {
+                    operator_to_slash.insert(id);
+                });
+            }
+
+            // Update the head receipt number
+            if let Some(next_head_receipt_number) = next_head_receipt_number {
+                HeadReceiptNumber::<T>::insert(domain_id, next_head_receipt_number);
+            }
+
+            // Slash operator who have submitted the pruned fraudulent ER
+            do_slash_operators::<T, _>(operator_to_slash.into_iter()).map_err(Error::<T>::from)?;
+
+            Self::deposit_event(Event::FraudProofProcessed {
+                domain_id,
+                new_head_receipt_number: next_head_receipt_number,
+            });
 
             Ok(())
         }
