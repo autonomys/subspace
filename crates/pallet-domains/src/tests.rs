2fc076c17f04fa7cd1fd5036e6971e17e83c46c7
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-09-13 13:29:58 +0200

    add fraud proof verification and generation test.
    At the moment, only verification tests are enabled since generation test's infra is still disabled.

--- crates/pallet-domains/src/tests.rs
+++ crates/pallet-domains/src/tests.rs
@@ -1,5 +1,6 @@
 use crate::block_tree::DomainBlock;
 use crate::domain_registry::{DomainConfig, DomainObject};
+use crate::pallet::StateRoots;
 use crate::{
     self as pallet_domains, BalanceOf, BlockTree, BundleError, Config, ConsensusBlockHash,
     DomainBlocks, DomainRegistry, ExecutionInbox, ExecutionReceiptOf, FraudProofError,
@@ -7,22 +8,28 @@ use crate::{
 };
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::dispatch::RawOrigin;
+use frame_support::storage::generator::StorageValue;
 use frame_support::traits::{ConstU16, ConstU32, ConstU64, Currency, Hooks};
 use frame_support::weights::Weight;
 use frame_support::{assert_err, assert_ok, parameter_types, PalletId};
 use scale_info::TypeInfo;
 use sp_core::crypto::Pair;
+use sp_core::storage::StorageKey;
 use sp_core::{Get, H256, U256};
-use sp_domains::fraud_proof::FraudProof;
+use sp_domains::fraud_proof::{FraudProof, InvalidBundleProof, InvalidTotalRewardsProof};
 use sp_domains::merkle_tree::MerkleTree;
 use sp_domains::{
     BundleHeader, DomainId, DomainInstanceData, DomainsHoldIdentifier, ExecutionReceipt,
     GenerateGenesisStateRoot, GenesisReceiptExtension, OpaqueBundle, OperatorId, OperatorPair,
-    ProofOfElection, RuntimeType, SealedBundleHeader, StakingHoldIdentifier,
+    ProofOfElection, ReceiptHash, RuntimeType, SealedBundleHeader, StakingHoldIdentifier,
 };
 use sp_runtime::testing::Header;
 use sp_runtime::traits::{AccountIdConversion, BlakeTwo256, Hash as HashT, IdentityLookup, Zero};
 use sp_runtime::OpaqueExtrinsic;
+use sp_state_machine::backend::AsTrieBackend;
+use sp_state_machine::{prove_read, Backend, TrieBackendBuilder};
+use sp_trie::trie_types::TrieDBMutBuilderV1;
+use sp_trie::{PrefixedMemoryDB, StorageProof, TrieMut};
 use sp_version::RuntimeVersion;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
@@ -745,6 +752,83 @@ fn test_invalid_fraud_proof() {
             Err(FraudProofError::BadReceiptNotFound)
         );
     });
+}
+
+#[test]
+fn test_invalid_total_rewards_fraud_proof() {
+    let creator = 0u64;
+    let operator_id = 1u64;
+    let head_domain_number = 10;
+    let mut ext = new_test_ext_with_extensions();
+    ext.execute_with(|| {
+        let domain_id = register_genesis_domain(creator, vec![operator_id]);
+        extend_block_tree(domain_id, operator_id, head_domain_number + 1);
+        assert_eq!(
+            HeadReceiptNumber::<Test>::get(domain_id),
+            head_domain_number
+        );
+
+        let bad_receipt_at = 8;
+        let bad_receipt = get_block_tree_node_at::<Test>(domain_id, bad_receipt_at)
+            .unwrap()
+            .execution_receipt;
+
+        let bad_receipt_hash = bad_receipt.hash();
+        let (fraud_proof, root) = generate_invalid_total_rewards_fraud_proof::<Test>(
+            domain_id,
+            bad_receipt_hash,
+            bad_receipt_at,
+            bad_receipt.domain_block_hash,
+            // set different reward in the storage and generate proof for that value
+            bad_receipt.total_rewards + 1,
+        );
+
+        StateRoots::<Test>::insert(
+            (domain_id, bad_receipt_at, bad_receipt.domain_block_hash),
+            root,
+        );
+        assert_ok!(Domains::validate_fraud_proof(&fraud_proof),);
+    });
+}
+
+fn generate_invalid_total_rewards_fraud_proof<T: Config>(
+    domain_id: DomainId,
+    bad_receipt_hash: ReceiptHash,
+    domain_number: T::BlockNumber,
+    domain_block_hash: T::Hash,
+    rewards: BalanceOf<T>,
+) -> (FraudProof<T::BlockNumber, T::Hash>, T::Hash) {
+    let storage_key =
+        sp_domains::fraud_proof::OperatorBlockRewards::storage_value_final_key().to_vec();
+    let mut root = T::Hash::default();
+    let mut mdb = PrefixedMemoryDB::<T::Hashing>::default();
+    {
+        let mut trie = TrieDBMutBuilderV1::new(&mut mdb, &mut root).build();
+        trie.insert(&storage_key, &rewards.encode()).unwrap();
+    };
+
+    let backend = TrieBackendBuilder::new(mdb, root).build();
+    let (root, storage_proof) = storage_proof_for_key::<T, _>(backend, StorageKey(storage_key));
+    (
+        FraudProof::InvalidBundle(InvalidBundleProof::TotalRewards(InvalidTotalRewardsProof {
+            domain_id,
+            bad_receipt_hash,
+            block_number: domain_number,
+            domain_block_hash,
+            storage_proof,
+        })),
+        root,
+    )
+}
+
+fn storage_proof_for_key<T: Config, B: Backend<T::Hashing> + AsTrieBackend<T::Hashing>>(
+    backend: B,
+    key: StorageKey,
+) -> (T::Hash, StorageProof) {
+    let state_version = sp_runtime::StateVersion::default();
+    let root = backend.storage_root(std::iter::empty(), state_version).0;
+    let proof = StorageProof::new(prove_read(backend, &[key]).unwrap().iter_nodes().cloned());
+    (root, proof)
 }
 
 #[test]
