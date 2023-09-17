a0c4ccae7962b4133214ff4f74c386d6c71f79b3
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-09-17 20:36:50 +0200

    move total rewards fraud proof verification to domain primitives

--- crates/sp-domains/src/verification.rs
+++ crates/sp-domains/src/verification.rs
@@ -1,16 +1,16 @@
+use crate::ExecutionReceipt;
 use frame_support::PalletError;
 use hash_db::Hasher;
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
 use sp_core::storage::StorageKey;
+use sp_runtime::traits::{Block, NumberFor};
 use sp_std::marker::PhantomData;
 use sp_trie::{read_trie_value, LayoutV1, StorageProof};
 
 /// Verification error.
 #[derive(Debug, PartialEq, Eq, Encode, Decode, PalletError, TypeInfo)]
 pub enum VerificationError {
-    /// Emits when the expected state root doesn't exist
-    InvalidStateRoot,
     /// Emits when the given storage proof is invalid.
     InvalidProof,
     /// Value doesn't exist in the Db for the given key.
@@ -36,4 +36,47 @@ impl<H: Hasher> StorageProofVerifier<H> {
 
         Ok(decoded)
     }
+}
+
+pub fn verify_invalid_total_rewards_fraud_proof<
+    CBlock,
+    DomainNumber,
+    DomainHash,
+    Balance,
+    Hashing,
+>(
+    bad_receipt: ExecutionReceipt<
+        NumberFor<CBlock>,
+        CBlock::Hash,
+        DomainNumber,
+        DomainHash,
+        Balance,
+    >,
+    storage_proof: &StorageProof,
+) -> Result<(), VerificationError>
+where
+    CBlock: Block,
+    Balance: PartialEq + Decode,
+    Hashing: Hasher<Out = CBlock::Hash>,
+    DomainHash: Encode,
+{
+    let state_root = bad_receipt.final_state_root.encode();
+    let state_root = CBlock::Hash::decode(&mut state_root.as_slice())
+        .map_err(|_| VerificationError::FailedToDecode)?;
+    let storage_key = StorageKey(crate::fraud_proof::operator_block_rewards_final_key());
+    let storage_proof = storage_proof.clone();
+
+    let total_rewards = StorageProofVerifier::<Hashing>::verify_and_get_value::<Balance>(
+        &state_root,
+        storage_proof,
+        storage_key,
+    )
+    .map_err(|_| VerificationError::InvalidProof)?;
+
+    // if the rewards matches, then this is an invalid fraud proof since rewards must be different.
+    if bad_receipt.total_rewards == total_rewards {
+        return Err(VerificationError::InvalidProof);
+    }
+
+    Ok(())
 }
