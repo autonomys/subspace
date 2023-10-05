f47e423aa7a11a40f151b4f6064a631ca7bbfe4d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2023-10-05 11:34:02 +0300

    Remove unnecessary `.collect()`

--- crates/subspace-farmer/src/single_disk_farm/farming.rs
+++ crates/subspace-farmer/src/single_disk_farm/farming.rs
@@ -166,8 +166,6 @@ where
 
                     Some((sector_index, audit_results.solution_candidates))
                 })
-                .collect::<Vec<_>>()
-                .into_iter()
                 .filter_map(|(sector_index, solution_candidates)| {
                     let sector_solutions = match solution_candidates.into_solutions(
                         &reward_address,
