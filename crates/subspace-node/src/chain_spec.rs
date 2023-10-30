d2a467b100d84fef09ffa39f2b75b191c3129080
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2023-10-31 00:34:04 +0200

    Add Gemini 3g chain spec

--- crates/subspace-node/src/chain_spec.rs
+++ crates/subspace-node/src/chain_spec.rs
@@ -43,6 +43,7 @@ use subspace_runtime_primitives::{AccountId, Balance, BlockNumber, SSC};
 
 const SUBSPACE_TELEMETRY_URL: &str = "wss://telemetry.subspace.network/submit/";
 const DEVNET_CHAIN_SPEC: &[u8] = include_bytes!("../res/chain-spec-raw-devnet.json");
+const GEMINI_3G_CHAIN_SPEC: &[u8] = include_bytes!("../res/chain-spec-raw-gemini-3g.json");
 
 /// List of accounts which should receive token grants, amounts are specified in SSC.
 const TOKEN_GRANTS: &[(&str, u128)] = &[
@@ -207,7 +208,7 @@ pub fn gemini_3g_compiled() -> Result<ConsensusChainSpec<RuntimeGenesisConfig>, 
 }
 
 pub fn gemini_3g_config() -> Result<ConsensusChainSpec<RuntimeGenesisConfig>, String> {
-    unimplemented!("Please use release prefixed with Gemini-3g.")
+    ConsensusChainSpec::from_json_bytes(GEMINI_3G_CHAIN_SPEC)
 }
 
 pub fn devnet_config() -> Result<ConsensusChainSpec<RuntimeGenesisConfig>, String> {
