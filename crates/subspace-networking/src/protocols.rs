3bb2da312c8db8e3910d44491d8b6e387726e4ae
Shamil Gadelshin <shamilgadelshin@gmail.com>
2023-12-14 18:32:41 +0700

    Remove connected-peers and peer-info protocols.

--- crates/subspace-networking/src/protocols.rs
+++ crates/subspace-networking/src/protocols.rs
@@ -1,6 +1,4 @@
 pub(crate) mod autonat_wrapper;
-pub(crate) mod connected_peers;
-pub mod peer_info;
 pub mod request_response;
 pub(crate) mod reserved_peers;
 pub(crate) mod subspace_connection_limits;
