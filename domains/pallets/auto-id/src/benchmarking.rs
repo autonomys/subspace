cd31007356a1e09db37143af8aec1f9cc453d2ba
vedhavyas <vedhavyas.singareddi@gmail.com>
2024-07-05 10:34:41 +0530

    bencamrk certficiation revocation for issuer and leaf

--- domains/pallets/auto-id/src/benchmarking.rs
+++ domains/pallets/auto-id/src/benchmarking.rs
@@ -56,6 +56,13 @@ mod benchmarks {
             .unwrap()
     }
 
+    fn do_register_leaf<T: Config>() {
+        let account = account("leaf", 0, 0);
+        let leaf_register_params = get_leaf_register_params();
+        Pallet::<T>::register_auto_id(RawOrigin::Signed(account).into(), leaf_register_params)
+            .unwrap()
+    }
+
     #[benchmark]
     fn register_issuer_auto_id() {
         let issuer_register_params = get_issuer_register_params();
@@ -75,6 +82,51 @@ mod benchmarks {
 
         #[extrinsic_call]
         register_auto_id(RawOrigin::Signed(account), leaf_register_params);
+    }
+
+    #[benchmark]
+    fn revoke_issuer_auto_id() {
+        let account = account("issuer", 0, 0);
+        pallet_timestamp::Pallet::<T>::set_timestamp(1_719_792_000_000u64);
+        do_register_issuer::<T>();
+
+        let issuer_id = Identifier::decode(
+            &mut include_bytes!("../res/benchmarks/issuer_auto_id")
+                .to_vec()
+                .as_slice(),
+        )
+        .unwrap();
+
+        let signature = Signature::decode(
+            &mut include_bytes!("../res/benchmarks/issuer_auto_id_revocation_signature").as_slice(),
+        )
+        .unwrap();
+
+        #[extrinsic_call]
+        revoke_certificate(RawOrigin::Signed(account), issuer_id, signature);
+    }
+
+    #[benchmark]
+    fn revoke_leaf_auto_id() {
+        let account = account("leaf", 0, 0);
+        pallet_timestamp::Pallet::<T>::set_timestamp(1_719_792_000_000u64);
+        do_register_issuer::<T>();
+        do_register_leaf::<T>();
+
+        let leaf_id = Identifier::decode(
+            &mut include_bytes!("../res/benchmarks/leaf_auto_id")
+                .to_vec()
+                .as_slice(),
+        )
+        .unwrap();
+
+        let signature = Signature::decode(
+            &mut include_bytes!("../res/benchmarks/leaf_auto_id_revocation_signature").as_slice(),
+        )
+        .unwrap();
+
+        #[extrinsic_call]
+        revoke_certificate(RawOrigin::Signed(account), leaf_id, signature);
     }
 
     impl_benchmark_test_suite!(Pallet, tests::new_test_ext(), tests::Test,);
