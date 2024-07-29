def9968c24047be449785ab0d3557eb0e93492ab
linning <linningde25@gmail.com>
2024-07-29 21:07:51 +0800

    Bump the evm and auto-id domain runtime spec version
    Signed-off-by: linning <linningde25@gmail.com>

--- domains/runtime/auto-id/src/lib.rs
+++ domains/runtime/auto-id/src/lib.rs
@@ -120,7 +120,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subspace-auto-id-domain"),
     impl_name: create_runtime_str!("subspace-auto-id-domain"),
     authoring_version: 0,
-    spec_version: 0,
+    spec_version: 1,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 0,
--- domains/runtime/evm/src/lib.rs
+++ domains/runtime/evm/src/lib.rs
@@ -247,7 +247,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subspace-evm-domain"),
     impl_name: create_runtime_str!("subspace-evm-domain"),
     authoring_version: 0,
-    spec_version: 1,
+    spec_version: 2,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 0,
