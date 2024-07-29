7d52dbb55570fa7c047af3ed1733ca1b3ca30f0e
linning <linningde25@gmail.com>
2024-07-29 21:07:03 +0800

    Add pallet-utility to the evm and auto-id domain runtime
    Signed-off-by: linning <linningde25@gmail.com>

--- domains/runtime/auto-id/src/lib.rs
+++ domains/runtime/auto-id/src/lib.rs
@@ -469,6 +469,13 @@ impl pallet_domain_sudo::Config for Runtime {
     type IntoRuntimeCall = IntoRuntimeCall;
 }
 
+impl pallet_utility::Config for Runtime {
+    type RuntimeEvent = RuntimeEvent;
+    type RuntimeCall = RuntimeCall;
+    type PalletsOrigin = OriginCaller;
+    type WeightInfo = pallet_utility::weights::SubstrateWeight<Runtime>;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 //
 // NOTE: Currently domain runtime does not naturally support the pallets with inherent extrinsics.
@@ -481,6 +488,7 @@ construct_runtime!(
         //  extrinsic.
         Timestamp: pallet_timestamp = 1,
         ExecutivePallet: domain_pallet_executive = 2,
+        Utility: pallet_utility = 8,
 
         // monetary stuff
         Balances: pallet_balances = 20,
