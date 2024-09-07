201756839b974bb971c631e520c82f79ea1d5f6e
tediou5 <qiangezaici@outlook.com>
2024-09-08 00:35:24 +0800

    feat: derive sub farm_ids from a farm_id

--- crates/subspace-farmer/src/farm.rs
+++ crates/subspace-farmer/src/farm.rs
@@ -28,6 +28,8 @@ use thiserror::Error;
 use ulid::Ulid;
 
 pub mod plotted_pieces;
+#[cfg(test)]
+mod tests;
 
 /// Erased error type
 pub type FarmError = Box<dyn std::error::Error + Send + Sync + 'static>;
@@ -498,6 +500,19 @@ impl FarmId {
     #[inline]
     pub fn new() -> Self {
         Self::Ulid(Ulid::new())
+    }
+
+    /// Derive sub IDs
+    #[inline]
+    pub fn derive_sub_ids(&self, n: usize) -> Vec<Self> {
+        match self {
+            FarmId::Ulid(ulid) => {
+                let ulid = ulid.0;
+                (0..n as u128)
+                    .map(|i| FarmId::Ulid(Ulid(ulid + i)))
+                    .collect()
+            }
+        }
     }
 }
 
---
+++ crates/subspace-farmer/src/farm/tests.rs
@@ -0,0 +1,19 @@
+use crate::farm::FarmId;
+
+#[test]
+fn derive_sub_farm_ids_test() {
+    let id = FarmId::new();
+    let sub_ids = id.derive_sub_ids(128);
+    assert_eq!(sub_ids.len(), 128);
+
+    match id {
+        FarmId::Ulid(id) => {
+            let id: u128 = id.into();
+            sub_ids.into_iter().zip(0..128u128).for_each(|(sub_id, i)| {
+                let FarmId::Ulid(sub_id) = sub_id;
+                let sub_id: u128 = sub_id.into();
+                assert_eq!(sub_id, id + i);
+            });
+        }
+    };
+}
