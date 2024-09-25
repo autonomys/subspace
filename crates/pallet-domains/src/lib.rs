61c406b597eec7a36ae37b83fad4fa2e1bb1302f
NingLin-P <linningde25@gmail.com>
2024-09-25 16:37:57 +0800

    Update crates/pallet-domains/src/lib.rs
    Co-authored-by: Nazar Mokrynskyi <nazar@mokrynskyi.com>

--- crates/pallet-domains/src/lib.rs
+++ crates/pallet-domains/src/lib.rs
@@ -432,9 +432,9 @@ mod pallet {
         /// Hook to handle chain rewards.
         type OnChainRewards: OnChainRewards<BalanceOf<Self>>;
 
-        /// The max number of withdrawal of a given nominator exist in the same time,
-        /// after this limit is met, the nominator need to unlock the withdrawal before
-        /// requesting new withdrawal.
+        /// The max number of withdrawals per nominator that may exist at any time,
+        /// once this limit is reached, the nominator need to unlock the withdrawal
+        /// before requesting new withdrawal.
         #[pallet::constant]
         type WithdrawalLimit: Get<u32>;
     }
