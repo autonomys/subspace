9a739cefb311361ae1c8e8070b6db01e8299485c
Dariia Porechna <35382599+dariolina@users.noreply.github.com>
2024-10-10 13:29:37 +0200

    inject balances from json into chainspec

--- crates/subspace-node/src/chain_spec.rs
+++ crates/subspace-node/src/chain_spec.rs
@@ -20,6 +20,7 @@ use crate::chain_spec_utils::{chain_spec_properties, get_account_id_from_seed};
 use crate::domain::auto_id_chain_spec;
 use crate::domain::cli::{GenesisDomain, SpecId};
 use crate::domain::evm_chain_spec::{self};
+use crate::genesis_allocations::get_genesis_allocations;
 use sc_chain_spec::GenericChainSpec;
 use sc_service::ChainType;
 use sc_subspace_chain_specs::DEVNET_CHAIN_SPEC;
@@ -86,7 +87,7 @@ pub fn gemini_3h_compiled() -> Result<GenericChainSpec, String> {
             AccountId::from_ss58check("5DNwQTHfARgKoa2NdiUM51ZUow7ve5xG9S2yYdSbVQcnYxBA")
                 .expect("Wrong root account address");
 
-        let balances = vec![(sudo_account.clone(), 1_000 * SSC)];
+        let balances=get_genesis_allocations();
         serde_json::to_value(subspace_genesis_config(
             sudo_account.clone(),
             balances,
