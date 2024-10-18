5c04b27c85e24facc419dcd62f83a581cfc4e16c
teor <teor@riseup.net>
2024-10-18 11:17:37 +1000

    Support IPv6 in CORS rules

--- crates/subspace-node/src/commands/run/domain.rs
+++ crates/subspace-node/src/commands/run/domain.rs
@@ -190,8 +190,10 @@ pub(super) fn create_domain_configuration(
                 Cors::List(vec![
                     "http://localhost:*".into(),
                     "http://127.0.0.1:*".into(),
+                    "http://[::1]:*".into(),
                     "https://localhost:*".into(),
                     "https://127.0.0.1:*".into(),
+                    "https://[::1]:*".into(),
                     "https://polkadot.js.org".into(),
                 ])
             }
