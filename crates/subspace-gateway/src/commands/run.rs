781299dfce8483385570ac8ab0af5ab75d0d31fa
teor <teor@riseup.net>
2024-10-18 11:16:43 +1000

    Basic gateway RPC and run implementation

--- crates/subspace-gateway/src/commands/run.rs
+++ crates/subspace-gateway/src/commands/run.rs
@@ -2,25 +2,34 @@
 //! This is the primary command for the gateway.
 
 mod dsn;
+mod rpc;
 
 use crate::commands::run::dsn::NetworkArgs;
+use crate::commands::run::rpc::{launch_rpc_server, RpcOptions, RPC_DEFAULT_PORT};
 use crate::commands::shutdown_signal;
+use anyhow::anyhow;
 use clap::Parser;
 use futures::{select, FutureExt};
+use std::env;
+use std::num::NonZeroUsize;
 use std::pin::pin;
-use std::{env, future};
+use subspace_core_primitives::pieces::Record;
+use subspace_data_retrieval::object_fetcher::ObjectFetcher;
+use subspace_erasure_coding::ErasureCoding;
+use subspace_gateway_rpc::{SubspaceGatewayRpc, SubspaceGatewayRpcConfig};
+use subspace_networking::utils::piece_provider::{NoPieceValidator, PieceProvider};
 use tracing::info;
 
 /// Options for running a node
 #[derive(Debug, Parser)]
-pub struct RunOptions {
+pub(crate) struct RunOptions {
     #[clap(flatten)]
     gateway: GatewayOptions,
 }
 
 /// Options for running a gateway
 #[derive(Debug, Parser)]
-pub(super) struct GatewayOptions {
+pub(crate) struct GatewayOptions {
     /// Enable development mode.
     ///
     /// Implies following flags (unless customized):
@@ -29,7 +38,12 @@ pub(super) struct GatewayOptions {
     dev: bool,
 
     #[clap(flatten)]
-    dsn: NetworkArgs,
+    dsn_options: NetworkArgs,
+
+    /// Options for RPC
+    #[clap(flatten)]
+    rpc_options: RpcOptions<RPC_DEFAULT_PORT>,
+    // TODO: maximum object size
 }
 
 /// Default run command for gateway
@@ -38,13 +52,18 @@ pub async fn run(run_options: RunOptions) -> anyhow::Result<()> {
     let signal = shutdown_signal();
 
     let RunOptions {
-        gateway: GatewayOptions { dev, mut dsn },
+        gateway:
+            GatewayOptions {
+                dev,
+                mut dsn_options,
+                rpc_options,
+            },
     } = run_options;
 
     // Development mode handling is limited to this section
     {
         if dev {
-            dsn.allow_private_ips = true;
+            dsn_options.allow_private_ips = true;
         }
     }
 
@@ -52,10 +71,22 @@ pub async fn run(run_options: RunOptions) -> anyhow::Result<()> {
     info!("✌️  version {}", env!("CARGO_PKG_VERSION"));
     info!("❤️  by {}", env!("CARGO_PKG_AUTHORS"));
 
-    let (_dsn_node, mut dsn_node_runner) = dsn::configure_network(dsn)?;
+    let erasure_coding = ErasureCoding::new(
+        NonZeroUsize::new(Record::NUM_S_BUCKETS.next_power_of_two().ilog2() as usize)
+            .expect("Not zero; qed"),
+    )
+    .map_err(|error| anyhow!("Failed to instantiate erasure coding: {error}"))?;
+
+    let (dsn_node, mut dsn_node_runner) = dsn::configure_network(dsn_options).await?;
     let dsn_fut = dsn_node_runner.run();
 
-    let rpc_fut = future::pending::<()>();
+    // TODO: implement piece validation
+    let piece_provider = PieceProvider::new(dsn_node, NoPieceValidator);
+    let object_fetcher = ObjectFetcher::new(piece_provider, erasure_coding, None);
+
+    let rpc_api = SubspaceGatewayRpc::new(SubspaceGatewayRpcConfig { object_fetcher });
+    let rpc_handle = launch_rpc_server(rpc_api, rpc_options).await?;
+    let rpc_fut = rpc_handle.stopped();
 
     // This defines order in which things are dropped
     let dsn_fut = dsn_fut;
