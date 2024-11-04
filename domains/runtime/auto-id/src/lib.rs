ca0e643bf92a2fb5de6256a14546308671852bc4
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2024-11-04 18:30:02 +0200

    Reset runtime spec version

--- domains/runtime/auto-id/src/lib.rs
+++ domains/runtime/auto-id/src/lib.rs
@@ -118,7 +118,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("subspace-auto-id-domain"),
     impl_name: Cow::Borrowed("subspace-auto-id-domain"),
     authoring_version: 0,
-    spec_version: 1,
+    spec_version: 0,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 0,
