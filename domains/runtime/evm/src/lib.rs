2092c19c5ba381f1e2bc47a5caea0100e2bfac28
vedhavyas <vedhavyas.singareddi@gmail.com>
2025-02-03 20:23:13 +0530

    bump spec and transaction version

--- domains/runtime/evm/src/lib.rs
+++ domains/runtime/evm/src/lib.rs
@@ -257,10 +257,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("subspace-evm-domain"),
     impl_name: Cow::Borrowed("subspace-evm-domain"),
     authoring_version: 0,
-    spec_version: 0,
+    spec_version: 1,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 0,
+    transaction_version: 1,
     system_version: 2,
 };
 
