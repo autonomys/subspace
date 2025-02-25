ebce7457e0699466af9a07471ae0d7b07fca7b76
teor <teor@riseup.net>
2025-02-26 08:36:17 +1000

    Update the registry and migration to use fallback decoding

--- crates/pallet-domains/src/migration_v2_to_v3.rs
+++ crates/pallet-domains/src/migration_v2_to_v3.rs
@@ -4,10 +4,27 @@
 //! If we add EVM or AutoId domains to Mainnet, also deploy it there.
 //! (If there are no EVM or AutoId domains, this migration does nothing, so it's safe to run.)
 
+// Types that have changed are imported or declared with a V2/V3 suffix.
+// Types that haven't changed are imported or declared without a suffix.
+pub(crate) use registry_v2::DomainRegistry as DomainRegistryV2;
+
+use crate::domain_registry::{DomainConfig, DomainObject as DomainObjectV3};
+use crate::pallet::DomainRegistry as DomainRegistryV3;
+use crate::runtime_registry::DomainRuntimeInfo as DomainRuntimeInfoV3;
 use crate::{Config, Pallet};
+use codec::{Decode, Encode};
+use domain_runtime_primitives::EVMChainId;
 use frame_support::migrations::VersionedMigration;
+use frame_support::pallet_prelude::TypeInfo;
 use frame_support::traits::UncheckedOnRuntimeUpgrade;
 use frame_support::weights::Weight;
+use sp_domains::DomainId;
+use sp_domains::{
+    AutoIdDomainRuntimeConfig as AutoIdDomainRuntimeConfigV3,
+    // changed in V3, but we use Into to convert to it
+    // DomainRuntimeConfig as DomainRuntimeConfigV3,
+    EvmDomainRuntimeConfig as EvmDomainRuntimeConfigV3,
+};
 
 pub type VersionCheckedMigrateDomainsV2ToV3<T> = VersionedMigration<
     2,
@@ -24,52 +41,77 @@ impl<T: Config> UncheckedOnRuntimeUpgrade for VersionUncheckedMigrateV2ToV3<T> {
     }
 }
 
-mod domain_registry_structure_migration {
-    // Types that have changed are imported or declared with a V2/V3 suffix.
-    // Types that haven't changed are imported or declared without a suffix.
-    pub(super) use crate::domain_registry::{DomainConfig, DomainObject as DomainObjectV3};
-    pub(super) use crate::pallet::DomainRegistry as DomainRegistryV3;
-    pub(super) use crate::runtime_registry::DomainRuntimeInfo as DomainRuntimeInfoV3;
-    use crate::{BalanceOf, BlockNumberFor, Config, Pallet, ReceiptHashFor};
-    use codec::{Decode, Encode};
-    use domain_runtime_primitives::EVMChainId;
-    use frame_support::pallet_prelude::{OptionQuery, TypeInfo, Weight};
+/// Domain runtime specific information to create domain raw genesis.
+#[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq, Copy)]
+#[allow(clippy::upper_case_acronyms)]
+pub enum DomainRuntimeInfoV2 {
+    EVM { chain_id: EVMChainId },
+    AutoId,
+}
+
+#[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq)]
+pub struct DomainObjectV2<Number, ReceiptHash, AccountId: Ord, Balance> {
+    /// The address of the domain creator, used to validate updating the domain config.
+    pub owner_account_id: AccountId,
+    /// The consensus chain block number when the domain first instantiated.
+    pub created_at: Number,
+    /// The hash of the genesis execution receipt for this domain.
+    pub genesis_receipt_hash: ReceiptHash,
+    /// The domain config.
+    pub domain_config: DomainConfig<AccountId, Balance>,
+    /// Domain runtime specific information.
+    pub domain_runtime_info: DomainRuntimeInfoV2,
+    /// The amount of balance hold on the domain owner account
+    pub domain_instantiation_deposit: Balance,
+}
+
+impl<T, U, V: Ord, W> From<DomainObjectV2<T, U, V, W>> for DomainObjectV3<T, U, V, W> {
+    fn from(domain_object_v2: DomainObjectV2<T, U, V, W>) -> Self {
+        let DomainObjectV2 {
+            owner_account_id,
+            created_at,
+            genesis_receipt_hash,
+            domain_config,
+            domain_runtime_info: domain_runtime_info_v2,
+            domain_instantiation_deposit,
+        } = domain_object_v2;
+
+        DomainObjectV3 {
+            owner_account_id,
+            created_at,
+            genesis_receipt_hash,
+            domain_config,
+            domain_runtime_info: domain_runtime_info_v2.into(),
+            domain_instantiation_deposit,
+        }
+    }
+}
+
+impl From<DomainRuntimeInfoV2> for DomainRuntimeInfoV3 {
+    fn from(domain_runtime_info_v2: DomainRuntimeInfoV2) -> Self {
+        match domain_runtime_info_v2 {
+            DomainRuntimeInfoV2::EVM { chain_id } => DomainRuntimeInfoV3::Evm {
+                chain_id,
+                // Added in V3
+                domain_runtime_config: EvmDomainRuntimeConfigV3::default(),
+            },
+            DomainRuntimeInfoV2::AutoId => DomainRuntimeInfoV3::AutoId {
+                // Added in V3
+                domain_runtime_config: AutoIdDomainRuntimeConfigV3::default(),
+            },
+        }
+    }
+}
+
+mod registry_v2 {
+    use super::*;
+    use crate::{BalanceOf, Config, Pallet, ReceiptHashFor};
+    use frame_support::pallet_prelude::OptionQuery;
     use frame_support::{storage_alias, Identity};
-    use sp_core::Get;
-    pub(super) use sp_domains::{
-        AutoIdDomainRuntimeConfig as AutoIdDomainRuntimeConfigV3,
-        DomainId,
-        // changed in V3, but we use Into to convert to it
-        // DomainRuntimeConfig as DomainRuntimeConfigV3,
-        EvmDomainRuntimeConfig as EvmDomainRuntimeConfigV3,
-    };
-
-    /// Domain runtime specific information to create domain raw genesis.
-    #[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq, Copy)]
-    #[allow(clippy::upper_case_acronyms)]
-    pub enum DomainRuntimeInfoV2 {
-        EVM { chain_id: EVMChainId },
-        AutoId,
-    }
-
-    #[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq)]
-    pub struct DomainObjectV2<Number, ReceiptHash, AccountId: Ord, Balance> {
-        /// The address of the domain creator, used to validate updating the domain config.
-        pub owner_account_id: AccountId,
-        /// The consensus chain block number when the domain first instantiated.
-        pub created_at: Number,
-        /// The hash of the genesis execution receipt for this domain.
-        pub genesis_receipt_hash: ReceiptHash,
-        /// The domain config.
-        pub domain_config: DomainConfig<AccountId, Balance>,
-        /// Domain runtime specific information.
-        pub domain_runtime_info: DomainRuntimeInfoV2,
-        /// The amount of balance hold on the domain owner account
-        pub domain_instantiation_deposit: Balance,
-    }
+    use frame_system::pallet_prelude::BlockNumberFor;
 
     #[storage_alias]
-    pub(super) type DomainRegistry<T: Config> = StorageMap<
+    pub(crate) type DomainRegistry<T: Config> = StorageMap<
         Pallet<T>,
         Identity,
         DomainId,
@@ -81,6 +123,13 @@ mod domain_registry_structure_migration {
         >,
         OptionQuery,
     >;
+}
+
+mod domain_registry_structure_migration {
+    use super::{DomainObjectV2, DomainObjectV3, DomainRegistryV3};
+    use crate::{BalanceOf, BlockNumberFor, Config, ReceiptHashFor};
+    use frame_support::pallet_prelude::Weight;
+    use sp_core::Get;
 
     pub(super) fn migrate_domain_registry_structure<T: Config>() -> Weight {
         let mut domain_count = 0;
@@ -91,31 +140,7 @@ mod domain_registry_structure_migration {
         >(|domain_object_v2| {
             domain_count += 1;
 
-            let domain_runtime_info_v3 = match domain_object_v2.domain_runtime_info {
-                DomainRuntimeInfoV2::EVM { chain_id } => {
-                    DomainRuntimeInfoV3::Evm {
-                        chain_id,
-                        // Added in V3
-                        domain_runtime_config: EvmDomainRuntimeConfigV3::default(),
-                    }
-                }
-                DomainRuntimeInfoV2::AutoId => {
-                    DomainRuntimeInfoV3::AutoId {
-                        // Added in V3
-                        domain_runtime_config: AutoIdDomainRuntimeConfigV3::default(),
-                    }
-                }
-            };
-
-            Some(DomainObjectV3 {
-                owner_account_id: domain_object_v2.owner_account_id,
-                created_at: domain_object_v2.created_at,
-                genesis_receipt_hash: domain_object_v2.genesis_receipt_hash,
-                domain_config: domain_object_v2.domain_config,
-                // Modified in V3
-                domain_runtime_info: domain_runtime_info_v3,
-                domain_instantiation_deposit: domain_object_v2.domain_instantiation_deposit,
-            })
+            Some(DomainObjectV3::from(domain_object_v2))
         });
 
         // 1 read and 1 write per domain
@@ -125,11 +150,8 @@ mod domain_registry_structure_migration {
 
 #[cfg(test)]
 mod tests {
-    use super::domain_registry_structure_migration::{
-        migrate_domain_registry_structure, AutoIdDomainRuntimeConfigV3, DomainConfig, DomainId,
-        DomainObjectV2, DomainObjectV3, DomainRegistry, DomainRegistryV3, DomainRuntimeInfoV2,
-        DomainRuntimeInfoV3, EvmDomainRuntimeConfigV3,
-    };
+    use super::domain_registry_structure_migration::*;
+    use super::*;
     use crate::tests::{new_test_ext, Test};
     use sp_domains::{EvmType as EvmTypeV3, OperatorAllowList};
 
@@ -155,33 +177,35 @@ mod tests {
             domain_runtime_info: DomainRuntimeInfoV2::EVM { chain_id },
             domain_instantiation_deposit: 9u32.into(),
         };
-
-        ext.execute_with(|| DomainRegistry::<Test>::set(domain_id, Some(domain.clone())));
-
-        ext.commit_all().unwrap();
+        let expected_migration = DomainObjectV3 {
+            owner_account_id: domain.owner_account_id,
+            created_at: domain.created_at,
+            genesis_receipt_hash: domain.genesis_receipt_hash,
+            domain_config: domain_config.clone(),
+            domain_runtime_info: DomainRuntimeInfoV3::Evm {
+                chain_id,
+                domain_runtime_config: EvmDomainRuntimeConfigV3 {
+                    // All current EVM domains are public EVMs
+                    evm_type: EvmTypeV3::Public,
+                },
+            },
+            domain_instantiation_deposit: domain.domain_instantiation_deposit,
+        };
 
         ext.execute_with(|| {
+            DomainRegistryV2::<Test>::set(domain_id, Some(domain.clone()));
+
+            let fallback_outcome = Pallet::<Test>::domain_registry_fallback(domain_id);
+            assert_eq!(fallback_outcome.unwrap(), expected_migration);
+
             let weights = migrate_domain_registry_structure::<Test>();
             assert_eq!(
                 weights,
                 <Test as frame_system::Config>::DbWeight::get().reads_writes(1, 1),
             );
             assert_eq!(
-                DomainRegistryV3::<Test>::get(domain_id),
-                Some(DomainObjectV3 {
-                    owner_account_id: domain.owner_account_id,
-                    created_at: domain.created_at,
-                    genesis_receipt_hash: domain.genesis_receipt_hash,
-                    domain_config,
-                    domain_runtime_info: DomainRuntimeInfoV3::Evm {
-                        chain_id,
-                        domain_runtime_config: EvmDomainRuntimeConfigV3 {
-                            // All current EVM domains are public EVMs
-                            evm_type: EvmTypeV3::Public
-                        },
-                    },
-                    domain_instantiation_deposit: domain.domain_instantiation_deposit,
-                })
+                DomainRegistryV3::<Test>::get(domain_id).unwrap(),
+                expected_migration,
             );
         });
     }
@@ -207,30 +231,32 @@ mod tests {
             domain_runtime_info: DomainRuntimeInfoV2::AutoId,
             domain_instantiation_deposit: 19u32.into(),
         };
-
-        ext.execute_with(|| DomainRegistry::<Test>::set(domain_id, Some(domain.clone())));
-
-        ext.commit_all().unwrap();
+        let expected_migration = DomainObjectV3 {
+            owner_account_id: domain.owner_account_id,
+            created_at: domain.created_at,
+            genesis_receipt_hash: domain.genesis_receipt_hash,
+            domain_config,
+            domain_runtime_info: DomainRuntimeInfoV3::AutoId {
+                // There are no AutoId-specific config fields at this time
+                domain_runtime_config: AutoIdDomainRuntimeConfigV3 {},
+            },
+            domain_instantiation_deposit: domain.domain_instantiation_deposit,
+        };
 
         ext.execute_with(|| {
+            DomainRegistryV2::<Test>::set(domain_id, Some(domain.clone()));
+
+            let fallback_outcome = Pallet::<Test>::domain_registry_fallback(domain_id);
+            assert_eq!(fallback_outcome.unwrap(), expected_migration);
+
             let weights = migrate_domain_registry_structure::<Test>();
             assert_eq!(
                 weights,
                 <Test as frame_system::Config>::DbWeight::get().reads_writes(1, 1),
             );
             assert_eq!(
-                DomainRegistryV3::<Test>::get(domain_id),
-                Some(DomainObjectV3 {
-                    owner_account_id: domain.owner_account_id,
-                    created_at: domain.created_at,
-                    genesis_receipt_hash: domain.genesis_receipt_hash,
-                    domain_config,
-                    domain_runtime_info: DomainRuntimeInfoV3::AutoId {
-                        // There are no AutoId-specific config fields at this time
-                        domain_runtime_config: AutoIdDomainRuntimeConfigV3 {}
-                    },
-                    domain_instantiation_deposit: domain.domain_instantiation_deposit,
-                })
+                DomainRegistryV3::<Test>::get(domain_id).unwrap(),
+                expected_migration,
             );
         });
     }
