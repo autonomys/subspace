336787ec228b5db2d71157b920a5a8f14580336a
teor <teor@riseup.net>
2025-02-25 13:08:37 +1000

    Simplify migration code and make it clear which types are changed

--- crates/pallet-domains/src/migration_v2_to_v3.rs
+++ crates/pallet-domains/src/migration_v2_to_v3.rs
@@ -25,45 +25,24 @@ impl<T: Config> UncheckedOnRuntimeUpgrade for VersionUncheckedMigrateV2ToV3<T> {
 }
 
 mod domain_registry_structure_migration {
-    use crate::domain_registry::{DomainConfig as DomainConfigV3, DomainObject as DomainObjectV3};
-    use crate::pallet::DomainRegistry as DomainRegistryV3;
-    use crate::runtime_registry::DomainRuntimeInfo as DomainRuntimeInfoV3;
+    // Types that have changed are imported or declared with a V2/V3 suffix.
+    // Types that haven't changed are imported or declared without a suffix.
+    pub(super) use crate::domain_registry::{DomainConfig, DomainObject as DomainObjectV3};
+    pub(super) use crate::pallet::DomainRegistry as DomainRegistryV3;
+    pub(super) use crate::runtime_registry::DomainRuntimeInfo as DomainRuntimeInfoV3;
     use crate::{BalanceOf, BlockNumberFor, Config, Pallet, ReceiptHashFor};
     use codec::{Decode, Encode};
-    use domain_runtime_primitives::{EVMChainId, MultiAccountId};
+    use domain_runtime_primitives::EVMChainId;
     use frame_support::pallet_prelude::{OptionQuery, TypeInfo, Weight};
     use frame_support::{storage_alias, Identity};
-    use scale_info::prelude::string::String;
     use sp_core::Get;
-    use sp_domains::{
-        AutoIdDomainRuntimeConfig,
+    pub(super) use sp_domains::{
+        AutoIdDomainRuntimeConfig as AutoIdDomainRuntimeConfigV3,
         DomainId,
         // changed in V3, but we use Into to convert to it
         // DomainRuntimeConfig as DomainRuntimeConfigV3,
-        EvmDomainRuntimeConfig,
-        OperatorAllowList,
-        RuntimeId,
+        EvmDomainRuntimeConfig as EvmDomainRuntimeConfigV3,
     };
-    use sp_runtime::Vec;
-
-    #[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq)]
-    pub struct DomainConfigV2<AccountId: Ord, Balance> {
-        /// A user defined name for this domain, should be a human-readable UTF-8 encoded string.
-        pub domain_name: String,
-        /// A pointer to the `RuntimeRegistry` entry for this domain.
-        pub runtime_id: RuntimeId,
-        /// The max bundle size for this domain, may not exceed the system-wide `MaxDomainBlockSize` limit.
-        pub max_bundle_size: u32,
-        /// The max bundle weight for this domain, may not exceed the system-wide `MaxDomainBlockWeight` limit.
-        pub max_bundle_weight: Weight,
-        /// The probability of successful bundle in a slot (active slots coefficient). This defines the
-        /// expected bundle production rate, must be `> 0` and `≤ 1`.
-        pub bundle_slot_probability: (u64, u64),
-        /// Allowed operators to operate for this domain.
-        pub operator_allow_list: OperatorAllowList<AccountId>,
-        // Initial balances for Domain.
-        pub initial_balances: Vec<(MultiAccountId, Balance)>,
-    }
 
     /// Domain runtime specific information to create domain raw genesis.
     #[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq, Copy)]
@@ -82,7 +61,7 @@ mod domain_registry_structure_migration {
         /// The hash of the genesis execution receipt for this domain.
         pub genesis_receipt_hash: ReceiptHash,
         /// The domain config.
-        pub domain_config: DomainConfigV2<AccountId, Balance>,
+        pub domain_config: DomainConfig<AccountId, Balance>,
         /// Domain runtime specific information.
         pub domain_runtime_info: DomainRuntimeInfoV2,
         /// The amount of balance hold on the domain owner account
@@ -112,18 +91,18 @@ mod domain_registry_structure_migration {
         >(|domain_object_v2| {
             domain_count += 1;
 
-            let domain_runtime_info = match domain_object_v2.domain_runtime_info {
+            let domain_runtime_info_v3 = match domain_object_v2.domain_runtime_info {
                 DomainRuntimeInfoV2::EVM { chain_id } => {
                     DomainRuntimeInfoV3::Evm {
                         chain_id,
                         // Added in V3
-                        domain_runtime_config: EvmDomainRuntimeConfig::default(),
+                        domain_runtime_config: EvmDomainRuntimeConfigV3::default(),
                     }
                 }
                 DomainRuntimeInfoV2::AutoId => {
                     DomainRuntimeInfoV3::AutoId {
                         // Added in V3
-                        domain_runtime_config: AutoIdDomainRuntimeConfig::default(),
+                        domain_runtime_config: AutoIdDomainRuntimeConfigV3::default(),
                     }
                 }
             };
@@ -132,17 +111,9 @@ mod domain_registry_structure_migration {
                 owner_account_id: domain_object_v2.owner_account_id,
                 created_at: domain_object_v2.created_at,
                 genesis_receipt_hash: domain_object_v2.genesis_receipt_hash,
-                domain_config: DomainConfigV3 {
-                    domain_name: domain_object_v2.domain_config.domain_name,
-                    runtime_id: domain_object_v2.domain_config.runtime_id,
-                    max_bundle_size: domain_object_v2.domain_config.max_bundle_size,
-                    max_bundle_weight: domain_object_v2.domain_config.max_bundle_weight,
-                    bundle_slot_probability: domain_object_v2.domain_config.bundle_slot_probability,
-                    operator_allow_list: domain_object_v2.domain_config.operator_allow_list,
-                    initial_balances: domain_object_v2.domain_config.initial_balances,
-                },
+                domain_config: domain_object_v2.domain_config,
                 // Modified in V3
-                domain_runtime_info,
+                domain_runtime_info: domain_runtime_info_v3,
                 domain_instantiation_deposit: domain_object_v2.domain_instantiation_deposit,
             })
         });
@@ -155,35 +126,32 @@ mod domain_registry_structure_migration {
 #[cfg(test)]
 mod tests {
     use super::domain_registry_structure_migration::{
-        migrate_domain_registry_structure, DomainConfigV2, DomainObjectV2, DomainRegistry,
-        DomainRuntimeInfoV2,
+        migrate_domain_registry_structure, AutoIdDomainRuntimeConfigV3, DomainConfig, DomainId,
+        DomainObjectV2, DomainObjectV3, DomainRegistry, DomainRegistryV3, DomainRuntimeInfoV2,
+        DomainRuntimeInfoV3, EvmDomainRuntimeConfigV3,
     };
-    use crate::domain_registry::{DomainConfig as DomainConfigV3, DomainObject as DomainObjectV3};
-    use crate::pallet::DomainRegistry as DomainRegistryV3;
-    use crate::runtime_registry::DomainRuntimeInfo as DomainRuntimeInfoV3;
     use crate::tests::{new_test_ext, Test};
-    use sp_domains::{
-        AutoIdDomainRuntimeConfig, DomainId, EvmDomainRuntimeConfig, EvmType, OperatorAllowList,
-    };
+    use sp_domains::{EvmType as EvmTypeV3, OperatorAllowList};
 
     #[test]
     fn test_domain_registry_structure_migration_evm() {
         let mut ext = new_test_ext();
         let domain_id: DomainId = 0.into();
         let chain_id = 8u32.into();
+        let domain_config = DomainConfig {
+            domain_name: "test-evm-migrate".to_string(),
+            runtime_id: 3u32,
+            max_bundle_size: 4,
+            max_bundle_weight: 5.into(),
+            bundle_slot_probability: (6, 7),
+            operator_allow_list: OperatorAllowList::Anyone,
+            initial_balances: vec![],
+        };
         let domain = DomainObjectV2 {
             owner_account_id: 1u32.into(),
             created_at: 2u32.into(),
             genesis_receipt_hash: Default::default(),
-            domain_config: DomainConfigV2 {
-                domain_name: "test-evm-migrate".to_string(),
-                runtime_id: 3u32,
-                max_bundle_size: 4,
-                max_bundle_weight: 5.into(),
-                bundle_slot_probability: (6, 7),
-                operator_allow_list: OperatorAllowList::Anyone,
-                initial_balances: vec![],
-            },
+            domain_config: domain_config.clone(),
             domain_runtime_info: DomainRuntimeInfoV2::EVM { chain_id },
             domain_instantiation_deposit: 9u32.into(),
         };
@@ -204,20 +172,12 @@ mod tests {
                     owner_account_id: domain.owner_account_id,
                     created_at: domain.created_at,
                     genesis_receipt_hash: domain.genesis_receipt_hash,
-                    domain_config: DomainConfigV3 {
-                        domain_name: domain.domain_config.domain_name,
-                        runtime_id: domain.domain_config.runtime_id,
-                        max_bundle_size: domain.domain_config.max_bundle_size,
-                        max_bundle_weight: domain.domain_config.max_bundle_weight,
-                        bundle_slot_probability: domain.domain_config.bundle_slot_probability,
-                        operator_allow_list: domain.domain_config.operator_allow_list,
-                        initial_balances: domain.domain_config.initial_balances,
-                    },
+                    domain_config,
                     domain_runtime_info: DomainRuntimeInfoV3::Evm {
                         chain_id,
-                        domain_runtime_config: EvmDomainRuntimeConfig {
+                        domain_runtime_config: EvmDomainRuntimeConfigV3 {
                             // All current EVM domains are public EVMs
-                            evm_type: EvmType::Public
+                            evm_type: EvmTypeV3::Public
                         },
                     },
                     domain_instantiation_deposit: domain.domain_instantiation_deposit,
@@ -230,19 +190,20 @@ mod tests {
     fn test_domain_registry_structure_migration_auto_id() {
         let mut ext = new_test_ext();
         let domain_id: DomainId = 10.into();
+        let domain_config = DomainConfig {
+            domain_name: "test-auto-id-migrate".to_string(),
+            runtime_id: 13u32,
+            max_bundle_size: 14,
+            max_bundle_weight: 15.into(),
+            bundle_slot_probability: (16, 17),
+            operator_allow_list: OperatorAllowList::Anyone,
+            initial_balances: vec![],
+        };
         let domain = DomainObjectV2 {
             owner_account_id: 11u32.into(),
             created_at: 12u32.into(),
             genesis_receipt_hash: Default::default(),
-            domain_config: DomainConfigV2 {
-                domain_name: "test-auto-id-migrate".to_string(),
-                runtime_id: 13u32,
-                max_bundle_size: 14,
-                max_bundle_weight: 15.into(),
-                bundle_slot_probability: (16, 17),
-                operator_allow_list: OperatorAllowList::Anyone,
-                initial_balances: vec![],
-            },
+            domain_config: domain_config.clone(),
             domain_runtime_info: DomainRuntimeInfoV2::AutoId,
             domain_instantiation_deposit: 19u32.into(),
         };
@@ -263,18 +224,10 @@ mod tests {
                     owner_account_id: domain.owner_account_id,
                     created_at: domain.created_at,
                     genesis_receipt_hash: domain.genesis_receipt_hash,
-                    domain_config: DomainConfigV3 {
-                        domain_name: domain.domain_config.domain_name,
-                        runtime_id: domain.domain_config.runtime_id,
-                        max_bundle_size: domain.domain_config.max_bundle_size,
-                        max_bundle_weight: domain.domain_config.max_bundle_weight,
-                        bundle_slot_probability: domain.domain_config.bundle_slot_probability,
-                        operator_allow_list: domain.domain_config.operator_allow_list,
-                        initial_balances: domain.domain_config.initial_balances,
-                    },
+                    domain_config,
                     domain_runtime_info: DomainRuntimeInfoV3::AutoId {
                         // There are no AutoId-specific config fields at this time
-                        domain_runtime_config: AutoIdDomainRuntimeConfig {}
+                        domain_runtime_config: AutoIdDomainRuntimeConfigV3 {}
                     },
                     domain_instantiation_deposit: domain.domain_instantiation_deposit,
                 })
