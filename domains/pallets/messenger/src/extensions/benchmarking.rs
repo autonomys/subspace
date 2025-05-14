a7478cb6cf6f46c9374d69cbe5d77f9682e6cd61
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2025-05-14 21:40:37 +0530

    add benchmarks for XDM's coming from Consensus chain

--- domains/pallets/messenger/src/extensions/benchmarking.rs
+++ domains/pallets/messenger/src/extensions/benchmarking.rs
@@ -1,11 +1,43 @@
 //! Benchmarking for `pallet-messenger` extensions.
-use crate::{Config, Pallet as Messenger};
+
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
+use crate::pallet::ChainAllowlist;
+use crate::{Config, Pallet as Messenger, ValidatedRelayMessage};
+#[cfg(not(feature = "std"))]
+use alloc::collections::BTreeSet;
+#[cfg(not(feature = "std"))]
+use alloc::vec::Vec;
 use frame_benchmarking::v2::*;
 use frame_support::dispatch::{DispatchInfo, PostDispatchInfo};
+use frame_system::pallet_prelude::BlockNumberFor;
+use parity_scale_codec::{Decode, Encode};
 use scale_info::prelude::fmt;
-use sp_runtime::traits::Dispatchable;
+use sp_domains::{ChainId, ChannelId};
+use sp_messenger::messages::{ChannelOpenParamsV1, CrossDomainMessage};
+use sp_runtime::traits::{Dispatchable, Zero};
+#[cfg(feature = "std")]
+use std::collections::BTreeSet;
 
 pub struct Pallet<T: Config>(Messenger<T>);
+
+#[derive(Encode, Decode)]
+pub(crate) struct FromConsensusRelayMessage<T: Config> {
+    pub(crate) state_root: T::Hash,
+    pub(crate) xdm: CrossDomainMessage<BlockNumberFor<T>, T::Hash, T::MmrHash>,
+}
+
+// From consensus relay message channel open data.
+const FCRMCO: &'static [u8; 738] =
+    include_bytes!("./fixtures/from_consensus_relay_message_channel_open.data");
+
+// From consensus relay message.
+const FCRM: &'static [u8; 806] = include_bytes!("./fixtures/from_consensus_relay_message.data");
+
+// From consensus relay message response.
+const FCRMR: &'static [u8; 697] =
+    include_bytes!("./fixtures/from_consensus_relay_message_response.data");
 
 #[allow(clippy::multiple_bound_locations)]
 #[benchmarks(where
@@ -17,9 +49,64 @@ mod benchmarks {
     use frame_system::pallet_prelude::RuntimeCallFor;
 
     #[benchmark]
-    fn run_bench() {
+    fn from_consensus_relay_message_channel_open() {
+        let FromConsensusRelayMessage { state_root, xdm } =
+            FromConsensusRelayMessage::<T>::decode(&mut FCRMCO.as_slice()).unwrap();
         #[block]
-        {}
+        {
+            let ValidatedRelayMessage {
+                message,
+                should_init_channel,
+                ..
+            } = Messenger::<T>::validate_relay_message(&xdm, state_root).unwrap();
+
+            Messenger::<T>::pre_dispatch_relay_message(message, should_init_channel).unwrap();
+        }
+    }
+
+    #[benchmark]
+    fn from_consensus_relay_message() {
+        let FromConsensusRelayMessage { state_root, xdm } =
+            FromConsensusRelayMessage::<T>::decode(&mut FCRMCO.as_slice()).unwrap();
+
+        // channel init and open
+        let ValidatedRelayMessage {
+            message,
+            should_init_channel,
+            ..
+        } = Messenger::<T>::validate_relay_message(&xdm, state_root).unwrap();
+        Messenger::<T>::pre_dispatch_relay_message(message, should_init_channel).unwrap();
+        Messenger::<T>::do_open_channel(xdm.src_chain_id, xdm.channel_id).unwrap();
+
+        let FromConsensusRelayMessage { state_root, xdm } =
+            FromConsensusRelayMessage::<T>::decode(&mut FCRM.as_slice()).unwrap();
+
+        #[block]
+        {
+            let ValidatedRelayMessage {
+                message,
+                should_init_channel,
+                ..
+            } = Messenger::<T>::validate_relay_message(&xdm, state_root).unwrap();
+
+            Messenger::<T>::pre_dispatch_relay_message(message, should_init_channel).unwrap();
+        }
+    }
+
+    #[benchmark]
+    fn from_consensus_relay_message_response() {
+        let FromConsensusRelayMessage { state_root, xdm } =
+            FromConsensusRelayMessage::<T>::decode(&mut FCRMR.as_slice()).unwrap();
+
+        // channel init
+        set_channel_init_state::<T>(xdm.src_chain_id, xdm.channel_id);
+        #[block]
+        {
+            let ValidatedRelayMessage { message, .. } =
+                Messenger::<T>::validate_relay_message_response(&xdm, state_root).unwrap();
+
+            Messenger::<T>::pre_dispatch_relay_message_response(message).unwrap();
+        }
     }
 
     impl_benchmark_test_suite!(
@@ -27,4 +114,18 @@ mod benchmarks {
         crate::mock::chain_a::new_test_ext(),
         crate::mock::chain_a::Runtime,
     );
+}
+
+fn set_channel_init_state<T: Config>(dst_chain_id: ChainId, channel_id: ChannelId) {
+    let init_params = ChannelOpenParamsV1 {
+        max_outgoing_messages: 100,
+    };
+
+    crate::Pallet::<T>::channels(dst_chain_id, channel_id).unwrap_or_else(|| {
+        let list = BTreeSet::from([dst_chain_id]);
+        ChainAllowlist::<T>::put(list);
+        crate::Pallet::<T>::do_init_channel(dst_chain_id, init_params, None, true, Zero::zero())
+            .unwrap();
+        crate::Pallet::<T>::channels(dst_chain_id, channel_id).unwrap()
+    });
 }
