3c4d3e8b917d74ddcea07559a473a826da49096c
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2025-06-16 15:40:04 +0530

    de-duplicate pot_input derivation code

--- crates/sc-consensus-subspace/src/block_import.rs
+++ crates/sc-consensus-subspace/src/block_import.rs
@@ -22,6 +22,7 @@ use sc_consensus::StateAction;
 use sc_consensus::block_import::{
     BlockCheckParams, BlockImport, BlockImportParams, ForkChoiceStrategy, ImportResult,
 };
+use sc_proof_of_time::source::pot_next_slot_input;
 use sc_proof_of_time::verifier::PotVerifier;
 use sp_api::{ApiError, ApiExt, ProvideRuntimeApi};
 use sp_block_builder::BlockBuilder as BlockBuilderApi;
@@ -352,36 +353,6 @@ where
             return Err(Error::SlotMustIncrease(parent_slot, pre_digest.slot()));
         }
 
-        let runtime_api = self.client.runtime_api();
-        let parent_pot_parameters = runtime_api
-            .pot_parameters(parent_hash)
-            .map_err(|_| Error::ParentUnavailable(parent_hash, block_hash))?;
-
-        let pot_input = if block_number.is_one() {
-            PotNextSlotInput {
-                slot: parent_slot + Slot::from(1),
-                slot_iterations: parent_pot_parameters.slot_iterations(),
-                seed: self.pot_verifier.genesis_seed(),
-            }
-        } else {
-            PotNextSlotInput::derive(
-                parent_pot_parameters.slot_iterations(),
-                parent_slot,
-                parent_pre_digest.pot_info().proof_of_time(),
-                &parent_pot_parameters.next_parameters_change(),
-            )
-        };
-
-        // Ensure proof of time is valid according to parent block
-        if !self.pot_verifier.is_output_valid(
-            pot_input,
-            pre_digest.slot() - parent_slot,
-            pre_digest.pot_info().proof_of_time(),
-            parent_pot_parameters.next_parameters_change(),
-        ) {
-            return Err(Error::InvalidProofOfTime);
-        }
-
         let parent_subspace_digest_items = if block_number.is_one() {
             None
         } else {
@@ -412,6 +383,29 @@ where
         // For PoT justifications we only need to check the seed and number of checkpoints, the rest
         // was already checked during stateless block verification.
         {
+            let runtime_api = self.client.runtime_api();
+            let parent_pot_parameters = runtime_api
+                .pot_parameters(parent_hash)
+                .map_err(|_| Error::ParentUnavailable(parent_hash, block_hash))?;
+
+            let pot_input = pot_next_slot_input::<Block>(
+                parent_header.number(),
+                parent_slot,
+                &parent_pot_parameters,
+                self.pot_verifier.genesis_seed(),
+                parent_pre_digest.pot_info().proof_of_time(),
+            );
+
+            // Ensure proof of time is valid according to parent block
+            if !self.pot_verifier.is_output_valid(
+                pot_input,
+                pre_digest.slot() - parent_slot,
+                pre_digest.pot_info().proof_of_time(),
+                parent_pot_parameters.next_parameters_change(),
+            ) {
+                return Err(Error::InvalidProofOfTime);
+            }
+
             let Some(subspace_justification) = justifications
                 .as_ref()
                 .and_then(|justifications| {
