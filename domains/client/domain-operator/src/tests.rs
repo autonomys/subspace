69525d8b6329ac82faddafe79d729d4f795dc08d
teor <teor@riseup.net>
2025-07-14 09:16:37 +1000

    Temporarily disable unstable tests on macOS

--- domains/client/domain-operator/src/tests.rs
+++ domains/client/domain-operator/src/tests.rs
@@ -42,8 +42,8 @@ use sp_domain_digests::AsPredigest;
 use sp_domains::core_api::DomainCoreApi;
 use sp_domains::merkle_tree::MerkleTree;
 use sp_domains::{
-    BlockFees, Bundle, BundleValidity, ChainId, ChannelId, DomainsApi, HeaderHashingFor,
-    InboxedBundle, InvalidBundleType, OperatorPublicKey, PermissionedActionAllowedBy, Transfers,
+    BlockFees, BundleValidity, ChainId, ChannelId, DomainsApi, HeaderHashingFor, InboxedBundle,
+    InvalidBundleType, OperatorPublicKey, PermissionedActionAllowedBy, Transfers,
 };
 use sp_domains_fraud_proof::InvalidTransactionCode;
 use sp_domains_fraud_proof::fraud_proof::{
@@ -98,6 +98,8 @@ where
     }
 }
 
+// Only used in tests that are temporarily disabled on macOS due to instability (#3385)
+#[cfg_attr(target_os = "macos", expect(dead_code))]
 fn number_of(consensus_node: &MockConsensusNode, block_hash: Hash) -> u32 {
     consensus_node
         .client
@@ -1818,8 +1820,13 @@ async fn test_domain_block_deriving_from_multiple_bundles() {
     assert_eq!(domain_block_number, head_receipt_number);
 }
 
+// This test is more unstable on macOS
+// TODO: find and fix the source of the instability (#3385)
+#[cfg(not(target_os = "macos"))]
 #[tokio::test(flavor = "multi_thread")]
 async fn collected_receipts_should_be_on_the_same_branch_with_current_best_block() {
+    use sp_domains::Bundle;
+
     let directory = TempDir::new().expect("Must be able to create temporary directory");
 
     let mut builder = sc_cli::LoggerBuilder::new("");
@@ -6377,6 +6384,9 @@ async fn test_skip_empty_bundle_production() {
     assert_eq!(alice.client.info().best_number, domain_block_number + 1);
 }
 
+// This test is more unstable on macOS
+// TODO: find and fix the source of the instability (#3385)
+#[cfg(not(target_os = "macos"))]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_bad_receipt_chain() {
     let directory = TempDir::new().expect("Must be able to create temporary directory");
