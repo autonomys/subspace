423b6cc1c30a2940532a59730b6e937912a2a7a7
tediou5 <qiangezaici@outlook.com>
2025-07-29 17:24:16 +0800

    fix: now farmer piece cache sync percentage base on total cache size

--- crates/subspace-farmer/src/farmer_cache.rs
+++ crates/subspace-farmer/src/farmer_cache.rs
@@ -515,13 +515,15 @@ where
 
         // Store whatever correct pieces are immediately available after restart
         self.piece_caches.write().await.clone_from(&caches);
+        let stored_count = caches.stored_pieces().len();
 
         debug!(
+            %stored_count,
             count = %piece_indices_to_store.len(),
             "Identified piece indices that should be cached",
         );
 
-        let pieces_to_download_total = piece_indices_to_store.len();
+        let pieces_to_download_total = piece_indices_to_store.len() + stored_count;
         let piece_indices_to_store = piece_indices_to_store
             .into_values()
             .collect::<Vec<_>>()
@@ -532,7 +534,7 @@ where
             .map(|chunk| chunk.to_vec())
             .collect::<Vec<_>>();
 
-        let downloaded_pieces_count = AtomicUsize::new(0);
+        let downloaded_pieces_count = AtomicUsize::new(stored_count);
         let caches = Mutex::new(caches);
         self.handlers.progress.call_simple(&0.0);
         let piece_indices_to_store = piece_indices_to_store.into_iter().enumerate();
