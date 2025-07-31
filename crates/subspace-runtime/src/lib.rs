e94ba31872e69f8fd71becb873e133c3b311a1ba
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2025-07-31 12:20:24 +0530

    bump runtime spec version

--- crates/subspace-runtime/src/lib.rs
+++ crates/subspace-runtime/src/lib.rs
@@ -132,7 +132,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: Cow::Borrowed("subspace"),
     authoring_version: 0,
     // The spec version can be different on Taurus and Mainnet
-    spec_version: 4,
+    spec_version: 5,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 0,
