d0a0dc3981b4ad3d5bcee56323dde99d0614d827
teor <teor@riseup.net>
2025-10-14 12:08:39 +0200

    Preserve fuzzer exit status in scripts/run-fuzzer-ci.sh

--- scripts/run-fuzzer-ci.sh
+++ scripts/run-fuzzer-ci.sh
@@ -7,4 +7,4 @@ sudo apt install -y protobuf-compiler binutils-dev
 
 cd ./fuzz/staking && cargo ziggy build --no-honggfuzz
 # cargo ziggy fuzz doesn't allow us to set a number of runs or a run time limit
-timeout 5m cargo ziggy fuzz --release
+timeout --preserve-status 5m cargo ziggy fuzz --release
