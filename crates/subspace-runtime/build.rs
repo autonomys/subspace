484126080263ee9caec22a4de20a82c7f47c491f
Jeremy Frank <jeremy@autonomys.xyz>
2025-10-27 14:59:13 -0600

    enable fn-dsa feature in wasm builds

--- crates/subspace-runtime/build.rs
+++ crates/subspace-runtime/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     #[cfg(feature = "std")]
     {
+        #[allow(unused_mut)]
         let mut builder = substrate_wasm_builder::WasmBuilder::new()
             .with_current_project()
             .export_heap_base()
